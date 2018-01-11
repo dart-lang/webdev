c4128bdeeba46ed5bdb4386e9fd19917bb87fba5
Nicholas Shahan <nshahan@google.com>
2018-01-11 11:27:47 -0800

    Specify allowed arguments.
    Actually passing all arguments was awkward because messages didn't reference webdev and args had to be passed with an extra `--`.

--- webdev/lib/src/command/build_command.dart
+++ webdev/lib/src/command/build_command.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 import 'package:io/io.dart';
 
-import 'package:args/args.dart';
 import 'package:args/command_runner.dart';
 
 /// Command to execute pub run build_runner build.
@@ -12,19 +11,25 @@ class BuildCommand extends Command {
   @override
   final description = 'Run builders to build a package.';
 
-  // TODO(nshahan) Remove once we are not just passing all arguments through.
-  final _anyArgs = new ArgParser(allowTrailingOptions: false);
-  @override
-  ArgParser get argParser => _anyArgs;
+  BuildCommand() {
+    // TODO(nshahan) Expose more args passed to build_runner build.
+    // build_runner might expose args for use in wrapping scripts like this one.
+    argParser
+      ..addOption('output',
+          abbr: 'o', help: 'A directory to write the result of a build to.')
+      ..addFlag('verbose',
+          abbr: 'v',
+          defaultsTo: false,
+          negatable: false,
+          help: 'Enables verbose logging.');
+  }
 
   @override
   Future run() async {
-    // TODO(nshahan) Is there a better way to pipe standard out/in?
-    // Would like to have consistent colors and spacing with build_runner.
     final manager = new ProcessManager();
     final executable = 'pub';
-    final arguments = ['run', 'build_runner', 'build'];
-    arguments.addAll(argResults.rest);
+    final arguments = ['run', 'build_runner', 'build', '--assume-tty'];
+    arguments.addAll(argResults.arguments);
     var spawn = await manager.spawn(executable, arguments);
 
     await spawn.exitCode;
