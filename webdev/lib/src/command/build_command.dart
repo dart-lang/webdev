7ce4e6a33b838bc7d626b43e5c34dfbf626b2bd7
Nicholas Shahan <nshahan@google.com>
2018-01-12 14:54:42 -0800

    Add initial support for build_runner serve (#4)
    Refactor shared arguments into abstract class.

--- webdev/lib/src/command/build_command.dart
+++ webdev/lib/src/command/build_command.dart
@@ -1,28 +1,16 @@
 import 'dart:async';
+
 import 'package:io/io.dart';
 
-import 'package:args/command_runner.dart';
+import 'build_runner_command_base.dart';
 
 /// Command to execute pub run build_runner build.
-class BuildCommand extends Command {
+class BuildCommand extends BuildRunnerCommandBase {
   @override
   final name = 'build';
 
   @override
   final description = 'Run builders to build a package.';
-
-  BuildCommand() {
-    // TODO(nshahan) Expose more args passed to build_runner build.
-    // build_runner might expose args for use in wrapping scripts like this one.
-    argParser
-      ..addOption('output',
-          abbr: 'o', help: 'A directory to write the result of a build to.')
-      ..addFlag('verbose',
-          abbr: 'v',
-          defaultsTo: false,
-          negatable: false,
-          help: 'Enables verbose logging.');
-  }
 
   @override
   Future run() async {
--- webdev/lib/src/webdev_command_runner.dart
+++ webdev/lib/src/webdev_command_runner.dart
@@ -1,13 +1,10 @@
 import 'package:args/command_runner.dart';
 
 import 'command/build_command.dart';
+import 'command/serve_command.dart';
 
 /// All available top level commands.
-CommandRunner webdevCommandRunner() {
-  final commandRunner =
-      new CommandRunner('webdev', 'A tool to develop Dart web projects.');
-
-  commandRunner.addCommand(new BuildCommand());
-
-  return commandRunner;
-}
+CommandRunner webdevCommandRunner() =>
+    new CommandRunner('webdev', 'A tool to develop Dart web projects.')
+      ..addCommand(new BuildCommand())
+      ..addCommand(new ServeCommand());
