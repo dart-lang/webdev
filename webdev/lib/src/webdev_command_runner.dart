775cd3bce2d1da2a0b33312562433ef371d0e123
Nicholas Shahan <nshahan@google.com>
2018-01-12 14:51:57 -0800

    Prefer cascade operator

--- webdev/lib/src/webdev_command_runner.dart
+++ webdev/lib/src/webdev_command_runner.dart
@@ -4,12 +4,7 @@ import 'command/build_command.dart';
 import 'command/serve_command.dart';
 
 /// All available top level commands.
-CommandRunner webdevCommandRunner() {
-  final commandRunner =
-      new CommandRunner('webdev', 'A tool to develop Dart web projects.');
-
-  commandRunner.addCommand(new BuildCommand());
-  commandRunner.addCommand(new ServeCommand());
-
-  return commandRunner;
-}
+CommandRunner webdevCommandRunner() =>
+    new CommandRunner('webdev', 'A tool to develop Dart web projects.')
+      ..addCommand(new BuildCommand())
+      ..addCommand(new ServeCommand());
