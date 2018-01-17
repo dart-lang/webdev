f7d07abacffdbcd1d0e1b9f84cb87c49bf58d8f7
Nicholas Shahan <nshahan@google.com>
2018-01-17 14:34:56 -0800

    Try getting dependencies if needed to run `pub run build_runner`

--- webdev/lib/src/command/serve_command.dart
+++ webdev/lib/src/command/serve_command.dart
@@ -1,5 +1,4 @@
 import 'dart:async';
-import 'dart:isolate';
 
 import 'build_runner_command_base.dart';
 
@@ -24,9 +23,6 @@ class ServeCommand extends BuildRunnerCommandBase {
   Future run() async {
     final arguments = ['serve'];
     arguments.addAll(argResults.arguments);
-    var exitPort = new ReceivePort();
-    await Isolate.spawnUri(await buildRunnerScript, arguments, null,
-        onExit: exitPort.sendPort, automaticPackageResolution: true);
-    await exitPort.first;
+    await runBuildRunner(arguments);
   }
 }
