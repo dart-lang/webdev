2a64f7a51d073e2581271b84a1dc69656bbae2f9
Kevin Moore <kevmoo@google.com>
2018-03-18 14:44:04 -0700

    Add missing copyright headers

--- example/web/main.dart
+++ example/web/main.dart
@@ -1,3 +1,7 @@
+// Copyright (c) 2018, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
 import 'package:angular/experimental.dart';
 
 // ignore: uri_has_not_been_generated
