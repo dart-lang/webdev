063b145264ed1a5f70e4e418b2046a3db861fc8f
Kevin Moore <kevmoo@users.noreply.github.com>
2018-04-10 16:07:43 -0700

    Fix dependencies in example (#36)

--- example/web/main.dart
+++ example/web/main.dart
@@ -2,12 +2,12 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-import 'package:angular/experimental.dart';
+import 'package:angular/angular.dart';
 
 // ignore: uri_has_not_been_generated
 import 'package:webdev_example_app/app_component.template.dart' as ng;
 
 main() {
   // ignore: argument_type_not_assignable
-  bootstrapFactory(ng.AppComponentNgFactory);
+  runApp(ng.AppComponentNgFactory);
 }
