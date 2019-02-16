2c67a5eeaaf817f45a5c2f09341f8cbd4d49b0f9
Gary Roumanis <grouma@google.com>
2019-02-15 17:06:42 -0800

    Launch in Chrome

--- webdev/lib/src/command/serve_command.dart
+++ webdev/lib/src/command/serve_command.dart
@@ -8,6 +8,7 @@ import 'dart:io';
 import 'package:build_daemon/client.dart';
 import 'package:build_daemon/data/build_target.dart';
 
+import '../serve/chrome.dart';
 import '../serve/daemon_client.dart';
 import '../serve/server_manager.dart';
 import '../serve/utils.dart';
@@ -15,10 +16,12 @@ import '../serve/webdev_server.dart';
 import 'command_base.dart';
 
 const _hostnameFlag = 'hostname';
-const _hotRestartFlag = 'hot-restart';
 const _hotReloadFlag = 'hot-reload';
+const _hotRestartFlag = 'hot-restart';
+const _launchInChrome = 'launch-in-chrome';
 const _liveReloadFlag = 'live-reload';
 const _logRequestsFlag = 'log-requests';
+
 final _defaultWebDirs = const ['web', 'test', 'example', 'benchmark'];
 
 Map<String, int> _parseDirectoryArgs(List<String> args) {
@@ -45,6 +48,9 @@ Map<String, int> _parseDirectoryArgs(List<String> args) {
 
 /// Command to run a server for local web development with the build daemon.
 class ServeCommand extends CommandBase {
+  ServerManager _serverManager;
+  Chrome _chrome;
+
   @override
   final name = 'serve';
 
@@ -56,24 +62,27 @@ class ServeCommand extends CommandBase {
     argParser
       ..addOption(_hostnameFlag,
           help: 'Specify the hostname to serve on', defaultsTo: 'localhost')
-      ..addFlag(_logRequestsFlag,
-          defaultsTo: false,
-          negatable: false,
-          help: 'Enables logging for each request to the server.')
-      ..addFlag(_liveReloadFlag,
-          defaultsTo: false,
-          negatable: false,
-          help:
-              'Automatically refreshes the page after each successful build.\n'
-              "Can't be used with $_hotRestartFlag")
+      ..addFlag(_hotReloadFlag, defaultsTo: false, negatable: false, hide: true)
       ..addFlag(_hotRestartFlag,
           defaultsTo: false,
           negatable: false,
           help: 'Automatically reloads changed modules after each build '
               'and restarts your application.\n'
               "Can't be used with $_liveReloadFlag")
-      ..addFlag(_hotReloadFlag,
-          defaultsTo: false, negatable: false, hide: true);
+      ..addFlag(_launchInChrome,
+          defaultsTo: false,
+          negatable: false,
+          help: 'Automatically launches your application in chrome')
+      ..addFlag(_liveReloadFlag,
+          defaultsTo: false,
+          negatable: false,
+          help:
+              'Automatically refreshes the page after each successful build.\n'
+              "Can't be used with $_hotRestartFlag")
+      ..addFlag(_logRequestsFlag,
+          defaultsTo: false,
+          negatable: false,
+          help: 'Enables logging for each request to the server.');
   }
 
   @override
@@ -84,10 +93,11 @@ class ServeCommand extends CommandBase {
     var workingDirectory = Directory.current.path;
 
     var hostname = argResults[_hostnameFlag] as String;
-    var logRequests = argResults[_logRequestsFlag] as bool;
-    var liveReload = argResults[_liveReloadFlag] as bool;
-    var hotRestart = argResults[_hotRestartFlag] as bool;
     var hotReload = argResults[_hotReloadFlag] as bool;
+    var hotRestart = argResults[_hotRestartFlag] as bool;
+    var launchInChrome = argResults[_launchInChrome] as bool;
+    var liveReload = argResults[_liveReloadFlag] as bool;
+    var logRequests = argResults[_logRequestsFlag] as bool;
     var verbose = argResults['verbose'] as bool;
 
     if (hotReload) {
@@ -150,16 +160,26 @@ class ServeCommand extends CommandBase {
       ));
     }
 
-    var manager = ServerManager(serverOptions, client.buildResults);
+    _serverManager = ServerManager(serverOptions, client.buildResults);
 
     print('Starting resource servers...');
-    await manager.start();
+    await _serverManager.start();
 
     print('Starting initial build...');
     client.startBuild();
 
+    if (launchInChrome) {
+      _chrome = await Chrome.start(_serverManager.uris);
+    }
+
     await client.finished;
-    await manager.stop();
+    await shutDown();
+
     return 0;
+  }
+
+  Future<void> shutDown() async {
+    await _serverManager?.stop();
+    await _chrome?.close();
   }
 }
--- webdev/lib/src/serve/utils.dart
+++ webdev/lib/src/serve/utils.dart
@@ -2,11 +2,39 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+import 'dart:async';
 import 'dart:io';
 
 import 'package:build_daemon/data/server_log.dart';
 import 'package:io/ansi.dart';
 import 'package:logging/logging.dart';
+
+/// Whether this computer supports binding to IPv6 addresses.
+var _maySupportIPv6 = true;
+
+/// Returns a port that is probably, but not definitely, not in use.
+///
+/// This has a built-in race condition: another process may bind this port at
+/// any time after this call has returned.
+Future<int> findUnusedPort() async {
+  int port;
+  if (_maySupportIPv6) {
+    try {
+      final socket = await ServerSocket.bind(InternetAddress.loopbackIPv6, 0,
+          v6Only: true);
+      port = socket.port;
+      await socket.close();
+    } on SocketException {
+      _maySupportIPv6 = false;
+    }
+  }
+  if (!_maySupportIPv6) {
+    final socket = await RawServerSocket.bind(InternetAddress.loopbackIPv4, 0);
+    port = socket.port;
+    await socket.close();
+  }
+  return port;
+}
 
 /// Colors and writes daemon [ServerLog]s
 void writeServerLog(ServerLog serverLog, bool verbose) {
