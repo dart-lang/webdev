1523d3257983f12daeb00ff076acd2bc2690daec
Jacob MacDonald <jakemac@google.com>
2019-02-26 13:57:18 -0800

    add a short delay before logging

--- dwds/example/hello_world/main.dart
+++ dwds/example/hello_world/main.dart
@@ -2,9 +2,13 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+import 'dart:async';
 import 'dart:html';
 
 void main() async {
+  // Travis needs a bit of time here.
+  await Future.delayed(Duration(seconds: 1));
+
   print('hello');
   window.console.error('Expected Error Log');
   throw ArgumentError('Expected Exception!');
