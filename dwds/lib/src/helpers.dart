3a2255b78ecc6149c73e96533586fa1ba62b4234
Gary Roumanis <groumanis@gmail.com>
2019-03-06 13:26:49 -0800

    Refactor DebugService (#179)

--- dwds/lib/src/helpers.dart
+++ dwds/lib/src/helpers.dart
@@ -2,6 +2,8 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+import 'dart:io';
+
 import 'package:vm_service_lib/vm_service_lib.dart';
 
 IsolateRef toIsolateRef(Isolate isolate) => IsolateRef()
@@ -16,4 +18,22 @@ int _nextId = 0;
 String createId() {
   _nextId++;
   return '$_nextId';
+}
+
+/// Returns a port that is probably, but not definitely, not in use.
+///
+/// This has a built-in race condition: another process may bind this port at
+/// any time after this call has returned.
+Future<int> findUnusedPort() async {
+  int port;
+  ServerSocket socket;
+  try {
+    socket =
+        await ServerSocket.bind(InternetAddress.loopbackIPv6, 0, v6Only: true);
+  } on SocketException {
+    socket = await ServerSocket.bind(InternetAddress.loopbackIPv4, 0);
+  }
+  port = socket.port;
+  await socket.close();
+  return port;
 }
