ea0791866a383a09b0944260eb2872bcf79278a3
Gary Roumanis <grouma@google.com>
2019-03-14 17:29:28 -0700

    Remove angular dep in example

--- example/web/main.dart
+++ example/web/main.dart
@@ -2,12 +2,6 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-import 'package:angular/angular.dart';
-
-// ignore: uri_has_not_been_generated
-import 'package:webdev_example_app/app_component.template.dart' as ng;
-
 void main() {
-  // ignore: argument_type_not_assignable
-  runApp(ng.AppComponentNgFactory);
+  print('Hello World!');
 }
