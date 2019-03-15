d0661d8baf823059c8103f21838a1d02e543aca3
Gary Roumanis <groumanis@gmail.com>
2019-03-14 18:10:07 -0700

    Refactor App Domain to not require a future ServerManager (#208)
    * Refactor App Domain to not require a future ServerManager

--- webdev/lib/src/daemon/app_domain.dart
+++ webdev/lib/src/daemon/app_domain.dart
@@ -7,10 +7,10 @@ import 'dart:io';
 
 import 'package:dwds/service.dart';
 import 'package:uuid/uuid.dart';
-import 'package:webdev/src/serve/chrome.dart';
-import 'package:webdev/src/serve/server_manager.dart';
 
+import '../serve/chrome.dart';
 import '../serve/debugger/webdev_vm_client.dart';
+import '../serve/server_manager.dart';
 import 'daemon.dart';
 import 'domain.dart';
 import 'utilites.dart';
@@ -23,57 +23,65 @@ class AppDomain extends Domain {
   DebugService _debugService;
   bool _isShutdown = false;
 
-  AppDomain(Daemon daemon, Future<ServerManager> futureServerManager)
+  void _initialize(ServerManager serverManager) async {
+    if (_isShutdown) return;
+
+    sendEvent('app.start', {
+      'appId': _appId,
+      'directory': Directory.current.path,
+      'deviceId': 'chrome',
+      'launchMode': 'run'
+    });
+
+    // TODO(https://github.com/dart-lang/webdev/issues/202) - Embed the appID
+    // in the WebServer.
+    var server = serverManager.servers.firstWhere((s) => s.target == 'web');
+    var devHandler = server.devHandler;
+    try {
+      await devHandler.connections.next;
+      // TODO(https://github.com/dart-lang/webdev/issues/202) - Remove.
+      await Future.delayed(Duration(seconds: 2));
+    } catch (e) {
+      sendEvent('daemon.logMessage',
+          {'level': 'severe', 'message': 'Unable to connect to the app: $e'});
+      return;
+    }
+
+    var chrome = await Chrome.connectedInstance;
+    // TODO(https://github.com/dart-lang/webdev/issues/202) - Run an eval to
+    // get the appId.
+    var appUrl = (await chrome.chromeConnection.getTabs())
+        .firstWhere((tab) => tab.url.startsWith('http://localhost'))
+        .url;
+
+    sendEvent('daemon.logMessage',
+        {'level': 'info', 'message': 'Connecting to $appUrl'});
+
+    _debugService =
+        await devHandler.startDebugService(chrome.chromeConnection, appUrl);
+    _webdevVmClient = await WebdevVmClient.create(_debugService);
+
+    sendEvent('app.debugPort', {
+      'appId': _appId,
+      'port': _debugService.port,
+      'wsUri': _debugService.wsUri,
+    });
+
+    // Shutdown could have been triggered while awaiting above.
+    // ignore: invariant_booleans
+    if (_isShutdown) dispose();
+
+    // TODO(grouma) - Add an event for when the application is started.
+  }
+
+  AppDomain(Daemon daemon, ServerManager serverManager)
       : _appId = Uuid().v1() as String,
         super(daemon, 'app') {
     registerHandler('restart', _restart);
     registerHandler('callServiceExtension', _callServiceExtension);
     registerHandler('stop', _stop);
 
-    futureServerManager.then((serverManager) async {
-      if (_isShutdown) return;
-      sendEvent('app.start', {
-        'appId': _appId,
-        'directory': Directory.current.path,
-        'deviceId': 'chrome',
-        'launchMode': 'run'
-      });
-
-      // TODO(https://github.com/dart-lang/webdev/issues/202) - Embed the appID
-      // in the WebServer.
-      var server = serverManager.servers.firstWhere((s) => s.target == 'web');
-      var devHandler = server.devHandler;
-      await devHandler.connections.next;
-      // TODO(https://github.com/dart-lang/webdev/issues/202) - Remove.
-      await Future.delayed(Duration(seconds: 2));
-
-      var chrome = await Chrome.connectedInstance;
-      // TODO(https://github.com/dart-lang/webdev/issues/202) - Run an eval to
-      // get the appId.
-      var appUrl = (await chrome.chromeConnection.getTabs())
-          .firstWhere(
-              (tab) => tab.url.startsWith('http://localhost:${server.port}'))
-          .url;
-
-      sendEvent('daemon.logMessage',
-          {'level': 'info', 'message': 'Connecting to $appUrl'});
-
-      _debugService = await server.devHandler
-          .startDebugService(chrome.chromeConnection, appUrl);
-      _webdevVmClient = await WebdevVmClient.create(_debugService);
-
-      sendEvent('app.debugPort', {
-        'appId': _appId,
-        'port': _debugService.port,
-        'wsUri': _debugService.wsUri,
-      });
-
-      // Shutdown could have been triggered while awaiting above.
-      // ignore: invariant_booleans
-      if (_isShutdown) dispose();
-
-      // TODO(grouma) - Add an event for when the application is started.
-    });
+    _initialize(serverManager);
   }
 
   Future<String> _callServiceExtension(Map<String, dynamic> args) {
