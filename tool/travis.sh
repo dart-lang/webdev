a145fc13d9b2ab6e7c5c1f247103eb41edc4d4b8
Kevin Moore <kevmoo@google.com>
2019-04-01 10:10:27 -0700

    use mono_repo v2, merge analyzer_and_format jobs

--- tool/travis.sh
+++ tool/travis.sh
@@ -1,55 +1,57 @@
 #!/bin/bash
-# Created with package:mono_repo v1.2.1
+# Created with package:mono_repo v2.0.0
 
-if [ -z "$PKG" ]; then
-  echo -e '\033[31mPKG environment variable must be set!\033[0m'
+if [[ -z ${PKGS} ]]; then
+  echo -e '\033[31mPKGS environment variable must be set!\033[0m'
   exit 1
 fi
 
-if [ "$#" == "0" ]; then
+if [[ "$#" == "0" ]]; then
   echo -e '\033[31mAt least one task argument must be provided!\033[0m'
   exit 1
 fi
 
-pushd $PKG
-pub upgrade || exit $?
-
 EXIT_CODE=0
 
-while (( "$#" )); do
-  TASK=$1
-  case $TASK in
-  dartanalyzer) echo
-    echo -e '\033[1mTASK: dartanalyzer\033[22m'
-    echo -e 'dartanalyzer --fatal-infos --fatal-warnings .'
-    dartanalyzer --fatal-infos --fatal-warnings . || EXIT_CODE=$?
-    ;;
-  dartfmt) echo
-    echo -e '\033[1mTASK: dartfmt\033[22m'
-    echo -e 'dartfmt -n --set-exit-if-changed .'
-    dartfmt -n --set-exit-if-changed . || EXIT_CODE=$?
-    ;;
-  test_0) echo
-    echo -e '\033[1mTASK: test_0\033[22m'
-    echo -e 'pub run test'
-    pub run test || EXIT_CODE=$?
-    ;;
-  test_1) echo
-    echo -e '\033[1mTASK: test_1\033[22m'
-    echo -e 'pub run test -j 1 -x requires-edge-sdk --run-skipped'
-    pub run test -j 1 -x requires-edge-sdk --run-skipped || EXIT_CODE=$?
-    ;;
-  test_2) echo
-    echo -e '\033[1mTASK: test_2\033[22m'
-    echo -e 'pub run test -t requires-edge-sdk --run-skipped'
-    pub run test -t requires-edge-sdk --run-skipped || EXIT_CODE=$?
-    ;;
-  *) echo -e "\033[31mNot expecting TASK '${TASK}'. Error!\033[0m"
-    EXIT_CODE=1
-    ;;
-  esac
+for PKG in ${PKGS}; do
+  echo -e "\033[1mPKG: ${PKG}\033[22m"
+  pushd "${PKG}" || exit $?
+  pub upgrade --no-precompile || exit $?
 
-  shift
+  for TASK in "$@"; do
+    case ${TASK} in
+    dartanalyzer) echo
+      echo -e '\033[1mTASK: dartanalyzer\033[22m'
+      echo -e 'dartanalyzer --fatal-infos --fatal-warnings .'
+      dartanalyzer --fatal-infos --fatal-warnings . || EXIT_CODE=$?
+      ;;
+    dartfmt) echo
+      echo -e '\033[1mTASK: dartfmt\033[22m'
+      echo -e 'dartfmt -n --set-exit-if-changed .'
+      dartfmt -n --set-exit-if-changed . || EXIT_CODE=$?
+      ;;
+    test_0) echo
+      echo -e '\033[1mTASK: test_0\033[22m'
+      echo -e 'pub run test'
+      pub run test || EXIT_CODE=$?
+      ;;
+    test_1) echo
+      echo -e '\033[1mTASK: test_1\033[22m'
+      echo -e 'pub run test -j 1 -x requires-edge-sdk --run-skipped'
+      pub run test -j 1 -x requires-edge-sdk --run-skipped || EXIT_CODE=$?
+      ;;
+    test_2) echo
+      echo -e '\033[1mTASK: test_2\033[22m'
+      echo -e 'pub run test -t requires-edge-sdk --run-skipped'
+      pub run test -t requires-edge-sdk --run-skipped || EXIT_CODE=$?
+      ;;
+    *) echo -e "\033[31mNot expecting TASK '${TASK}'. Error!\033[0m"
+      EXIT_CODE=1
+      ;;
+    esac
+  done
+
+  popd
 done
 
-exit $EXIT_CODE
+exit ${EXIT_CODE}
