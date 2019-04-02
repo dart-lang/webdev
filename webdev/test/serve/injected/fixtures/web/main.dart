9d0f390daf76147711d97c8eb0de190ac1da2359
Jacob MacDonald <jakemac@google.com>
2019-04-02 10:38:11 -0700

    support invoking the ext.flutter.disassemble extension method before a hot restart

--- webdev/test/serve/injected/fixtures/web/main.dart
+++ webdev/test/serve/injected/fixtures/web/main.dart
@@ -2,8 +2,16 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+import 'dart:developer';
 import 'dart:html';
 
 void main() {
-  document.body.appendText('Hello World!');
+  document.body.appendText('Hello World!!');
+
+  registerExtension('ext.flutter.disassemble', (_, __) async {
+    document.body.appendText('start disassemble ');
+    await Future.delayed(Duration(seconds: 1));
+    document.body.appendText('end disassemble ');
+    return ServiceExtensionResponse.result('{}');
+  });
 }
