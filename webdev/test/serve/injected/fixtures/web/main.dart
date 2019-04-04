c4fe24d2a7d23debe80b013047a2c0bc7c3a8eef
Alan Knight <alanknight@google.com>
2019-04-04 12:11:06 -0700

    Fix lint in serve

--- webdev/test/serve/injected/fixtures/web/main.dart
+++ webdev/test/serve/injected/fixtures/web/main.dart
@@ -11,7 +11,7 @@ void main() {
 
   registerExtension('ext.flutter.disassemble', (_, __) async {
     document.body.appendText('start disassemble ');
-    await Future.delayed(Duration(seconds: 1));
+    await Future.delayed(const Duration(seconds: 1));
     document.body.appendText('end disassemble ');
     return ServiceExtensionResponse.result('{}');
   });
