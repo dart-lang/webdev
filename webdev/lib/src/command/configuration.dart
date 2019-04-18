03e5f6e0c2cc485da2a97a67506b2342904b483b
Jacob MacDonald <jakemac53@gmail.com>
2019-04-18 14:25:30 -0700

    fix parsing of output dir arg

--- webdev/lib/src/command/configuration.dart
+++ webdev/lib/src/command/configuration.dart
@@ -169,12 +169,12 @@ class Configuration {
     String outputInput;
     if (output != 'NONE') {
       var splitOutput = output.split(':');
-      if (splitOutput.length == 2) {
-        outputInput = splitOutput.first;
-        outputPath = splitOutput.last;
-      } else {
+      if (splitOutput.length == 1) {
         outputInput = '';
         outputPath = output;
+      } else {
+        outputInput = splitOutput.first;
+        outputPath = splitOutput.skip(1).join(':');
       }
     }
 
