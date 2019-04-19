8005cf21ad6df1ac9c74b92283b075ffea6af7ad
Devon Carew <devoncarew@google.com>
2019-04-19 11:33:32 -0700

    Update daemon_command.dart

--- webdev/lib/src/command/daemon_command.dart
+++ webdev/lib/src/command/daemon_command.dart
@@ -56,12 +56,7 @@ class DaemonCommand extends Command<int> {
       daemon = Daemon(_stdinCommandStream, _stdoutCommandResponse);
       var daemonDomain = DaemonDomain(daemon);
       setLogHandler((level, message, {verbose}) {
-        if (verbose == true) {
-          daemonDomain.sendEvent(
-              'daemon.logMessage', {'level': '$level', 'message': message});
-        } else {
-          daemonDomain.sendEvent('daemon.log', {'log': message});
-        }
+        daemonDomain.sendEvent('daemon.log', {'log': message});
       });
       daemon.registerDomain(daemonDomain);
       var configuration =
