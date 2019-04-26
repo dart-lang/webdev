301c61bbf89ed3cb654392ddef064b220505859f
Kevin Moore <kevmoo@users.noreply.github.com>
2019-04-26 14:21:24 -0700

    Drop unused dep on pkg:built_value_generator (#343)
    Remove unused deps and rev build_daemon version

--- webdev/lib/src/pubspec.dart
+++ webdev/lib/src/pubspec.dart
@@ -125,7 +125,7 @@ class PubspecLock {
 
 Future<List<PackageExceptionDetails>> _validateBuildDaemonVersion(
     PubspecLock pubspecLock) async {
-  var buildDaemonConstraint = '^0.5.0';
+  var buildDaemonConstraint = '>=0.5.0 <0.7.0';
 
   var issues = <PackageExceptionDetails>[];
 
