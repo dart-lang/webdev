b14d73671502e6347a11fd2f64e3fbfc163ff555
Alan Knight <alanknight@google.com>
2019-05-07 12:39:14 -0700

    Don't overwrite the application id if it's already been set (#376)

--- webdev/web/client.dart
+++ webdev/web/client.dart
@@ -28,7 +28,8 @@ import 'reloading_manager.dart';
 // pub run build_runner build web
 Future<void> main() async {
   // Set the unique id for this instance of the app.
-  dartAppInstanceId = Uuid().v1();
+  // Test apps may already have this set.
+  dartAppInstanceId ??= Uuid().v1();
 
   var currentDigests = await _getDigests();
 
