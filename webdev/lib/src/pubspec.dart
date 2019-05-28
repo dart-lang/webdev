cd58de259cb2b034392e85b0516d80f8565fc569
Jacob MacDonald <jakemac@google.com>
2019-05-28 13:11:08 -0600

    Update to the latest build daemon (#399)
    Primarily this is updating to the new ServerLog api.
    
    I added git overrides for now but we could wait to merge this until publishing the build_daemon/build_runner packages, and so it can merge cleanly without overrides.

--- webdev/lib/src/pubspec.dart
+++ webdev/lib/src/pubspec.dart
@@ -140,7 +140,7 @@ class PubspecLock {
 
 Future<List<PackageExceptionDetails>> _validateBuildDaemonVersion(
     PubspecLock pubspecLock) async {
-  var buildDaemonConstraint = '>=0.5.0 <0.7.0';
+  var buildDaemonConstraint = '>=1.0.0 <2.0.0';
 
   var issues = <PackageExceptionDetails>[];
 
