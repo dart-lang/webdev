6e12833930d5398a0972809adc29878e4e38ab68
Gary Roumanis <grouma@google.com>
2019-06-25 13:11:45 -0700

    skip on windows

--- webdev/test/serve/injected/reload_test.dart
+++ webdev/test/serve/injected/reload_test.dart
@@ -99,7 +99,8 @@ void main() {
       expect(isolate.breakpoints.isEmpty, isTrue);
 
       await fixture.webdev.kill();
-    });
+      // TODO(sdk/issues/37364) - Remove once corresponding SDK issue is fixed.
+    }, skip: Platform.isWindows);
 
     test('can live reload changes ', () async {
       await fixture.buildAndLoad(['--auto=refresh']);
