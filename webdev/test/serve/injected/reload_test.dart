3770f7c42a546d88e6fa37708c29ca2a3a543623
Gary Roumanis <grouma@google.com>
2019-07-08 16:43:00 -0700

    Actually skip on windows

--- webdev/test/serve/injected/reload_test.dart
+++ webdev/test/serve/injected/reload_test.dart
@@ -57,6 +57,9 @@ void main() {
     });
 
     test('can hot restart while paused', () async {
+      // TODO(sdk/issues/37364) - Remove once corresponding SDK issue is fixed.
+      if (Platform.isWindows) return;
+
       await fixture.buildAndLoad(['--debug']);
 
       await fixture.webdriver.driver.keyboard.sendChord([Keyboard.alt, 'd']);
@@ -99,8 +102,7 @@ void main() {
       expect(isolate.breakpoints.isEmpty, isTrue);
 
       await fixture.webdev.kill();
-      // TODO(sdk/issues/37364) - Remove once corresponding SDK issue is fixed.
-    }, skip: Platform.isWindows);
+    });
 
     test('can live reload changes ', () async {
       await fixture.buildAndLoad(['--auto=refresh']);
