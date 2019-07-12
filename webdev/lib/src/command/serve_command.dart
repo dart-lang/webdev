05a024f31190c70bba38fd1172c2748e4f611edd
Pi Songkuntham <pisong@google.com>
2019-07-12 10:01:01 -0700

    Edited the option description for the extension backend.

--- webdev/lib/src/command/serve_command.dart
+++ webdev/lib/src/command/serve_command.dart
@@ -59,7 +59,7 @@ refresh: Performs a full page refresh.
         help: 'Enable the launching of DevTools (Alt + D / Option + D). '
             'This also enables --$launchInChromeFlag.')
     ..addFlag(debugExtensionFlag,
-        help: 'Enable the debug Chrome extension backend. ', hide: true)
+        help: 'Enable the backend for the Dart Debug Extension.', hide: true)
     ..addSeparator('Advanced:')
     ..addOption(chromeDebugPortFlag,
         help: 'Specify which port the Chrome debugger is listening on. '
