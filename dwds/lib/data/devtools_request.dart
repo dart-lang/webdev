6731062a5a8bd7748a5cc09a85f2d5b0c40162ab
Pi Songkuntham <pisong@princeton.edu>
2019-07-25 17:44:40 -0700

    Added tabUrl to DevToolsRequest. (#520)
    * Added tabUrl to DevToolsRequest.

--- dwds/lib/data/devtools_request.dart
+++ dwds/lib/data/devtools_request.dart
@@ -23,6 +23,10 @@ abstract class DevToolsRequest
 
   /// Identifies a given instance of an application, unique per tab/window.
   String get instanceId;
+
+  /// Belongs to the tab that requests the DevTools
+  @nullable
+  String get tabUrl;
 }
 
 /// A response to a [DevToolsRequest].
