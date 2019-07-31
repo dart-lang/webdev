cf95ba76057ac8f2962285ffdeeb9544862a35eb
Pi Songkuntham <pisong@google.com>
2019-07-31 13:35:29 -0700

    Cleaned.

--- dwds/lib/src/servers/extension_backend.dart
+++ dwds/lib/src/servers/extension_backend.dart
@@ -2,10 +2,8 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-import 'dart:async';
 import 'dart:io';
 
-import 'package:dwds/src/servers/extension_debugger.dart';
 import 'package:http_multi_server/http_multi_server.dart';
 import 'package:shelf/shelf_io.dart';
 import 'package:sse/server/sse_handler.dart';
@@ -13,11 +11,17 @@ import 'package:sse/server/sse_handler.dart';
 SseHandler _sseHandler = SseHandler(Uri.parse('/test'));
 
 /// A backend for the Dart Debug Extension which sets up an SSE handler.
+///
+/// The SSE handler simply sends a simple message and prints the response when
+/// a client connects.
 class ExtensionBackend {
   final String hostname;
   final int port;
   final HttpServer _server;
-  ExtensionBackend._(this.hostname, this.port, this._server);
+
+  ExtensionBackend._(this.hostname, this.port, this._server) {
+    _listen();
+  }
 
   // Starts the backend on an open port.
   static Future<ExtensionBackend> start() async {
@@ -30,7 +34,12 @@ class ExtensionBackend {
     await _server.close();
   }
 
-  var connections = _sseHandler.connections;
-  Future<ExtensionDebugger> get extensionDebugger async =>
-      ExtensionDebugger(await connections.next);
+  void _listen() async {
+    var connections = _sseHandler.connections;
+    while (await connections.hasNext) {
+      var connection = await connections.next;
+      connection.sink.add('foo');
+      connection.stream.listen(print);
+    }
+  }
 }
