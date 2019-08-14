515adc7cdc050c49fe1ffab41af4ffad1150140b
Alan Knight <alanknight@google.com>
2019-08-14 11:29:27 -0700

    Seems to work

--- dwds/lib/src/debugging/even_smaller.dart
+++ dwds/lib/src/debugging/even_smaller.dart
@@ -20,53 +20,44 @@ Future<List<Property>> visibleProperties(
   // We skip the global and the outer library scope and assume everything before
   // that is a method scope.
   var numberOfMethods = scopeList.length - 2;
-  var methodScopes = scopeList
+  var propertyLists = scopeList
       .sublist(0, numberOfMethods)
-      .map((x) => MethodScope(
+      .map((x) async => await _propertiesForScope(
           objectId: x['object']['objectId'] as String,
           callFrameId: callFrameId,
           debugger: debugger))
       .toList();
-  for (var scope in methodScopes) {
-    await scope.fetchProperties();
-  }
-  var properties = [for (var scope in methodScopes) ...scope.properties];
+  var properties = [for (var list in propertyLists) ...await list];
   return properties;
 }
 
-class MethodScope {
-  String objectId;
-  String callFrameId;
-
-  List<Property> properties;
-
-  Debugger debugger;
-
-  Property self;
-
-  MethodScope({this.objectId, this.callFrameId, this.debugger});
-
-  /// Retrieve the properties for this scope, including finding a `this` binding
-  /// if available, even if it's not included in the original Chrome scope
-  /// object.
-  Future<void> fetchProperties() async {
-    properties = await debugger.getProperties(objectId);
-    self = properties.firstWhere((x) => x.name == 'this', orElse: () => null);
-    self ??= await findMissingThis();
+/// Retrieve the properties for this scope, including finding a `this` binding
+/// if available, even if it's not included in the original Chrome scope
+/// object.
+Future<List<Property>> _propertiesForScope(
+    {String objectId, String callFrameId, Debugger debugger}) async {
+  var properties = await debugger.getProperties(objectId);
+  var self = properties.firstWhere((x) => x.name == 'this', orElse: () => null);
+  if (self == null) {
+    var syntheticSelf = await _findMissingThis(callFrameId, debugger);
+    if (syntheticSelf != null) {
+      properties.add(syntheticSelf);
+    }
   }
+  return properties;
+}
 
-  /// Fill in the 'this' scope if it wasn't in the original scope.
-  ///
-  /// If we were not given a 'this' value in the Chrome scopes that might mean
-  /// we're in a nested closure, or we might be a top-level function.
-  /// Construct a 'this'. If it turns out to be null/undefined, make it empty.
-  /// If it just duplicates the containing library, ignore it. Otherwise insert
-  /// it into the scope where it will get expanded normally.
-  Future<Property> findMissingThis() async {
-    if (self != null) return self;
-    // If 'this' is a library return null, otherwise
-    // return 'this'.
-    const findCurrent = '''
+/// Fill in the 'this' scope if it wasn't in the original scope.
+///
+/// If we were not given a 'this' value in the Chrome scopes that might mean
+/// we're in a nested closure, or we might be a top-level function.
+/// Construct a 'this'. If it turns out to be null/undefined, make it empty.
+/// If it just duplicates the containing library, ignore it. Otherwise insert
+/// it into the scope where it will get expanded normally.
+Future<Property> _findMissingThis(String callFrameId, Debugger debugger) async {
+  // If 'this' is a library return null, otherwise
+  // return 'this'.
+  const findCurrent = '''
         (function (THIS) { 
            if (THIS === window) { return null; }
            let dart = require('dart_sdk').dart
@@ -77,14 +68,12 @@ class MethodScope {
             }
             } return THIS; })(this)''';
 
-    var actualThis =
-        await debugger.evaluateJsOnCallFrame(callFrameId, findCurrent);
+  var actualThis =
+      await debugger.evaluateJsOnCallFrame(callFrameId, findCurrent);
 
-    if (actualThis.type != 'undefined') {
-      /// Construct a property for `this`.
-      self = Property({'name': 'this', 'value': actualThis});
-      properties.add(self);
-    }
-    return self;
+  if (actualThis.type != 'undefined') {
+    /// Construct a property for `this`.
+    return Property({'name': 'this', 'value': actualThis});
   }
+  return null;
 }
