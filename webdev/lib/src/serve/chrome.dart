0714544136174b7ef8462698337087d42b434701
Gary Roumanis <grouma@google.com>
2019-09-25 13:36:42 -0700

    Use browser launcher

--- webdev/lib/src/serve/chrome.dart
+++ webdev/lib/src/serve/chrome.dart
@@ -3,81 +3,31 @@
 // BSD-style license that can be found in the LICENSE file.
 
 import 'dart:async';
-import 'dart:convert';
-import 'dart:io';
 
-import 'package:path/path.dart' as p;
+import 'package:browser_launcher/browser_launcher.dart' as browser_launcher;
 import 'package:webkit_inspection_protocol/webkit_inspection_protocol.dart';
-
-import 'utils.dart';
-
-const _chromeEnvironment = 'CHROME_EXECUTABLE';
-const _linuxExecutable = 'google-chrome';
-const _macOSExecutable =
-    '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome';
-const _windowsExecutable = r'Google\Chrome\Application\chrome.exe';
-var _windowsPrefixes = [
-  Platform.environment['LOCALAPPDATA'],
-  Platform.environment['PROGRAMFILES'],
-  Platform.environment['PROGRAMFILES(X86)']
-];
-
-String get _executable {
-  if (Platform.environment.containsKey(_chromeEnvironment)) {
-    return Platform.environment[_chromeEnvironment];
-  }
-  if (Platform.isLinux) return _linuxExecutable;
-  if (Platform.isMacOS) return _macOSExecutable;
-  if (Platform.isWindows) {
-    return p.join(
-        _windowsPrefixes.firstWhere((prefix) {
-          if (prefix == null) return false;
-          var path = p.join(prefix, _windowsExecutable);
-          return File(path).existsSync();
-        }, orElse: () => '.'),
-        _windowsExecutable);
-  }
-  throw StateError('Unexpected platform type.');
-}
 
 var _currentCompleter = Completer<Chrome>();
 
 /// A class for managing an instance of Chrome.
 class Chrome {
   final int debugPort;
-  final Process _process;
-  final Directory _dataDir;
-
+  final browser_launcher.Chrome _chrome;
   final ChromeConnection chromeConnection;
 
   Chrome._(
     this.debugPort,
-    this.chromeConnection, {
-    Process process,
-    Directory dataDir,
-  })  : _process = process,
-        _dataDir = dataDir;
+    this._chrome,
+  ) : chromeConnection = _chrome.chromeConnection;
 
   Future<void> close() async {
     if (_currentCompleter.isCompleted) _currentCompleter = Completer<Chrome>();
-    chromeConnection.close();
-    _process?.kill(ProcessSignal.sigkill);
-    await _process?.exitCode;
-    try {
-      // Chrome starts another process as soon as it dies that modifies the
-      // profile information. Give it some time before attempting to delete
-      // the directory.
-      await Future.delayed(const Duration(milliseconds: 500));
-      await _dataDir?.delete(recursive: true);
-    } catch (_) {
-      // Silently fail if we can't clean up the profile information.
-      // It is a system tmp directory so it should get cleaned up eventually.
-    }
+    await _chrome.close();
   }
 
   /// Connects to an instance of Chrome with an open debug port.
-  static Future<Chrome> fromExisting(int port) async =>
-      _connect(Chrome._(port, ChromeConnection('localhost', port)));
+  static Future<Chrome> fromExisting(int port) async => _connect(
+      Chrome._(port, await browser_launcher.Chrome.fromExisting(port)));
 
   static Future<Chrome> get connectedInstance => _currentCompleter.future;
 
@@ -85,43 +35,9 @@ class Chrome {
   ///
   /// Each url in [urls] will be loaded in a separate tab.
   static Future<Chrome> start(List<String> urls, {int port}) async {
-    var dataDir = Directory.systemTemp.createTempSync();
-    port = port == null || port == 0 ? await findUnusedPort() : port;
-    var args = [
-      // Using a tmp directory ensures that a new instance of chrome launches
-      // allowing for the remote debug port to be enabled.
-      '--user-data-dir=${dataDir.path}',
-      '--remote-debugging-port=$port',
-      // When the DevTools has focus we don't want to slow down the application.
-      '--disable-background-timer-throttling',
-      // Since we are using a temp profile, disable features that slow the
-      // Chrome launch.
-      '--disable-extensions',
-      '--disable-popup-blocking',
-      '--bwsi',
-      '--no-first-run',
-      '--no-default-browser-check',
-      '--disable-default-apps',
-      '--disable-translate',
-    ]..addAll(urls);
-
-    var process = await Process.start(_executable, args);
-
-    // Wait until the DevTools are listening before trying to connect.
-    await process.stderr
-        .transform(utf8.decoder)
-        .transform(const LineSplitter())
-        .firstWhere((line) => line.startsWith('DevTools listening'))
-        .timeout(const Duration(seconds: 60),
-            onTimeout: () =>
-                throw Exception('Unable to connect to Chrome DevTools.'));
-
-    return _connect(Chrome._(
-      port,
-      ChromeConnection('localhost', port),
-      process: process,
-      dataDir: dataDir,
-    ));
+    var chrome =
+        await browser_launcher.Chrome.startWithDebugPort(urls, debugPort: port);
+    return _connect(Chrome._(chrome.debugPort, chrome));
   }
 
   static Future<Chrome> _connect(Chrome chrome) async {
@@ -131,7 +47,7 @@ class Chrome {
     // The connection is lazy. Try a simple call to make sure the provided
     // connection is valid.
     try {
-      await chrome.chromeConnection.getTabs();
+      await chrome._chrome.chromeConnection.getTabs();
     } catch (e) {
       await chrome.close();
       throw ChromeError(
