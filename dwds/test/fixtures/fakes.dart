f939dd50bf60bf04acbd99c11cbc851c7396b334
Alan Knight <alanknight@google.com>
2019-10-01 15:37:48 -0700

    Fix two null errors in toJson of vm protocol objects (#699)

--- dwds/test/fixtures/fakes.dart
+++ dwds/test/fixtures/fakes.dart
@@ -40,7 +40,8 @@ class FakeInspector extends Domain implements AppInspector {
   @override
   Future<ScriptList> getScripts(String isolateId) => null;
   @override
-  Future<ScriptRef> scriptRefFor(String uri) => null;
+  Future<ScriptRef> scriptRefFor(String uri) =>
+      Future.value(ScriptRef(id: 'fake', uri: 'fake://uri'));
   @override
   Future<ScriptRef> scriptWithId(String scriptId) => null;
   @override
