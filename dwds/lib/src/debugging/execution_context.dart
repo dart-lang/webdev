31c5e9d5bce36e0048354ac6ca49563b52e56b87
Gary Roumanis <grouma@google.com>
2019-12-04 13:07:09 -0800

    Use stream pattern

--- dwds/lib/src/debugging/execution_context.dart
+++ dwds/lib/src/debugging/execution_context.dart
@@ -4,6 +4,7 @@
 
 import 'dart:async';
 
+import 'package:async/async.dart';
 import 'package:dwds/src/debugging/remote_debugger.dart';
 
 /// The execution context in which to do remote evaluations.
@@ -11,14 +12,16 @@ class ExecutionContext {
   final RemoteDebugger _remoteDebugger;
 
   // Contexts that may contain a Dart application.“
-  final _contexts = <int>[];
+  StreamQueue<int> _contexts;
 
   int _id;
 
   /// Returns the context ID that contains the running Dart application.
   Future<int> get id async {
     if (_id != null) return _id;
-    for (var context in _contexts.toList()) {
+    while (await _contexts.hasNext
+        .timeout(const Duration(milliseconds: 50), onTimeout: () => false)) {
+      var context = await _contexts.next;
       var result =
           await _remoteDebugger.sendCommand('Runtime.evaluate', params: {
         'expression': r'window["$dartAppInstanceId"];',
@@ -36,15 +39,14 @@ class ExecutionContext {
   }
 
   ExecutionContext(this._id, this._remoteDebugger) {
+    var contextController = StreamController<int>();
     _remoteDebugger
         .eventStream('Runtime.executionContextsCleared', (e) => e)
-        .listen((_) {
-      _contexts.clear();
-      _id = null;
-    });
+        .listen((_) => _id = null);
     _remoteDebugger
         .eventStream('Runtime.executionContextCreated',
             (e) => int.parse(e.params['context']['id'].toString()))
-        .listen(_contexts.add);
+        .listen(contextController.add);
+    _contexts = StreamQueue(contextController.stream);
   }
 }
