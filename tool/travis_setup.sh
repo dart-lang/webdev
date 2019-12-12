0339366655c0835465e37f03d3b45a6e4d6b0e17
Gary Roumanis <grouma@google.com>
2019-12-12 09:55:55 -0800

    test this

--- tool/travis_setup.sh
+++ tool/travis_setup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-chromedriver.bat --version
 
 if [[ $TRAVIS_OS_NAME == "windows" ]]; then
     echo Installing Google Chrome Stable...
+    chromedriver.bat --version
     # Install Chrome via Chocolatey while `addons: chrome` doesn't seem to work on Windows yet
     # https://travis-ci.community/t/installing-google-chrome-stable-but-i-cant-find-it-anywhere/2118
     choco install googlechrome --acceptlicense --yes --no-progress --ignore-checksums
