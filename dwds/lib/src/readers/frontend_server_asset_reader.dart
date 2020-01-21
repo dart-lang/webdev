e1296871e712f2c4c4061da18d6d8ce7e453b6c3
Gary Roumanis <grouma@google.com>
2020-01-21 13:24:43 -0800

    FrontendServerAssetReader dartSourceContents

--- dwds/lib/src/readers/frontend_server_asset_reader.dart
+++ dwds/lib/src/readers/frontend_server_asset_reader.dart
@@ -2,10 +2,17 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+import 'dart:async';
 import 'dart:convert';
 import 'dart:io';
 
+import 'package:dwds/dwds.dart';
+import 'package:logging/logging.dart';
+import 'package:package_resolver/package_resolver.dart';
 import 'package:path/path.dart' as p;
+import 'package:shelf/shelf.dart';
+import 'package:shelf_packages_handler/shelf_packages_handler.dart';
+import 'package:shelf_static/shelf_static.dart';
 
 import 'asset_reader.dart';
 
@@ -16,8 +23,12 @@ class FrontendServerAssetReader implements AssetReader {
   final File _mapIncremental;
   final File _jsonOriginal;
   final File _jsonIncremental;
+  final LogWriter _logWriter;
 
-  // Map of Dart module server path to source map contents.
+  /// Handler for Dart resources.
+  final _sourceHandler = Completer<Handler>();
+
+  /// Map of Dart module server path to source map contents.
   final _mapContents = <String, String>{};
 
   bool _haveReadOriginals = false;
@@ -30,16 +41,51 @@ class FrontendServerAssetReader implements AssetReader {
   ///
   /// Corresponding `.json` and `.map` files will be read relative to
   /// [outputPath].
+  ///
+  /// [packageRoot] is the path to the directory that contains a `.packages`
+  /// file for the application.
   FrontendServerAssetReader(
     String outputPath,
+    String packageRoot,
+    this._logWriter,
   )   : _mapOriginal = File('$outputPath.map'),
         _mapIncremental = File('$outputPath.incremental.map'),
         _jsonOriginal = File('$outputPath.json'),
-        _jsonIncremental = File('$outputPath.incremental.json');
+        _jsonIncremental = File('$outputPath.incremental.json') {
+    () async {
+      var cascade = Cascade()
+          .add(packagesDirHandler(
+              resolver: await PackageResolver.loadConfig(
+                  p.toUri(p.join(packageRoot, '.packages')))))
+          .add(createStaticHandler(packageRoot));
+      _sourceHandler.complete(cascade.handler);
+    }();
+  }
 
   @override
-  Future<String> dartSourceContents(String serverPath) =>
-      throw UnimplementedError();
+  Future<String> dartSourceContents(String serverPath) async {
+    if (!serverPath.endsWith('.dart')) return null;
+    var sourceHandler = await _sourceHandler.future;
+
+    // We need a valid HTTP URI for the handler to properly resolve resources.
+    // The host and port don't actually matter though.
+    var response = await sourceHandler(
+        Request('GET', Uri.parse('http://localhost:8080/$serverPath')));
+
+    if (response.statusCode != HttpStatus.ok) {
+      _logWriter(Level.WARNING, '''
+      Failed to load asset at path: $serverPath.
+
+      Status code: ${response.statusCode}
+
+      Headers:
+      ${const JsonEncoder.withIndent('  ').convert(response.headers)}
+      ''');
+      return null;
+    } else {
+      return await response.readAsString();
+    }
+  }
 
   @override
   Future<String> sourceMapContents(String serverPath) async {
