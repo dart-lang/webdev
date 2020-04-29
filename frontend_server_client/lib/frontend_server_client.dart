f5d37733448f719e66913a04d8c10bbd96da04ab
Jacob MacDonald <jakemac@google.com>
2020-04-29 08:06:39 -0700

    add explicit show

--- frontend_server_client/lib/frontend_server_client.dart
+++ frontend_server_client/lib/frontend_server_client.dart
@@ -2,4 +2,4 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-export 'src/frontend_server_client.dart';
+export 'src/frontend_server_client.dart' show FrontendServerClient;
