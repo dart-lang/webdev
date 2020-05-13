736679bf9d480f3a3c54c223fddb8cb16d211a62
Jacob MacDonald <jakemac@google.com>
2020-05-13 07:52:10 -0700

    pass --verbose to frontend server if enabled

--- frontend_server_client/lib/src/frontend_server_client.dart
+++ frontend_server_client/lib/src/frontend_server_client.dart
@@ -75,6 +75,7 @@ class FrontendServerClient {
       '--packages=$packagesJson',
       if (enableHttpUris) '--enable-http-uris',
       '--incremental',
+      if (verbose) '--verbose',
     ]);
     var feServerStdoutLines = StreamQueue(feServer.stdout
         .transform(utf8.decoder)
