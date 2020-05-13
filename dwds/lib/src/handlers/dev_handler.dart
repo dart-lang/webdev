357a36d5241a84b7b176e8a361ca3279995d0904
Devon Carew <devoncarew@google.com>
2020-05-13 14:18:31 -0700

    Update dev_handler.dart (#1009)

--- dwds/lib/src/handlers/dev_handler.dart
+++ dwds/lib/src/handlers/dev_handler.dart
@@ -124,7 +124,8 @@ class DevHandler {
     WipConnection tabConnection;
     var appInstanceId = appConnection.request.instanceId;
     for (var tab in await chromeConnection.getTabs()) {
-      if (tab.url.startsWith('chrome-extensions:')) continue;
+      if (tab.isChromeExtension || tab.isBackgroundPage) continue;
+
       tabConnection = await tab.connect();
       var contextQueue = StreamQueue<int>(tabConnection.runtime.eventStream(
           'Runtime.executionContextCreated',
