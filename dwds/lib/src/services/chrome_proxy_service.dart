ba2cfbfa81c80e06623de96eeb67e986ee543afd
Graciliano Monteiro Passos <gracilianomp@gmail.com>
2020-06-10 08:04:24 -0300

    ErrorRef: Added required new parameter id

--- dwds/lib/src/services/chrome_proxy_service.dart
+++ dwds/lib/src/services/chrome_proxy_service.dart
@@ -387,7 +387,7 @@ ${globalLoadStrategy.loadModuleSnippet}("dart_sdk").developer.invokeExtension(
                 'to file a bug.');
           }
           return ErrorRef(
-              kind: 'error', message: '${result.type}: ${result.value}');
+              kind: 'error', message: '${result.type}: ${result.value}', id: result.objectId);
         }
         return _inspector?.instanceHelper?.instanceRefFor(result);
       } catch (e, s) {
