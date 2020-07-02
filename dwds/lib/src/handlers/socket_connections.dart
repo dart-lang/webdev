9cbf1bfe44254bd05ede41a30d340d13a322a0d3
Danny Tuppeny <danny@tuppeny.com>
2020-07-02 11:10:09 +0100

    Add copyright and comments

--- dwds/lib/src/handlers/socket_connections.dart
+++ dwds/lib/src/handlers/socket_connections.dart
@@ -1,3 +1,7 @@
+// Copyright (c) 2020, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
 import 'dart:async';
 
 import 'package:async/async.dart';
@@ -7,19 +11,29 @@ import 'package:shelf_web_socket/shelf_web_socket.dart';
 import 'package:sse/server/sse_handler.dart';
 import 'package:web_socket_channel/web_socket_channel.dart';
 
+/// An individual (transport-agnostic) bidirectional socket connection.
 abstract class SocketConnection {
+  /// Whether this connection is currently in the KeepAlive timeout period.
   bool get isInKeepAlivePeriod;
+
+  /// Messages added to the sink must be JSON encodable.
   StreamSink<dynamic> get sink;
+
   Stream<String> get stream;
+
+  /// Immediately close the connection, ignoring any keepAlive period.
   void shutdown();
 }
 
+/// A handler that accepts (transport-agnostic) bidirection socket connections.
 abstract class SocketHandler {
   StreamQueue<SocketConnection> get connections;
   FutureOr<Response> handler(Request request);
   void shutdown();
 }
 
+/// An implemenation of [SocketConnection] that users server-sent events (SSE)
+/// and HTTP POSTS for bidirectional communication by wrapping an [SseConnection].
 class SseSocketConnection extends SocketConnection {
   final SseConnection _connection;
 
@@ -35,6 +49,8 @@ class SseSocketConnection extends SocketConnection {
   void shutdown() => _connection.shutdown();
 }
 
+/// An implemenation of [SocketHandler] that accepts server-sent events (SSE)
+/// connections and wraps them in an [SseSocketConnection].
 class SseSocketHandler extends SocketHandler {
   final SseHandler _sseHandler;
   final StreamController<SseSocketConnection> _connectionsStream =
@@ -60,21 +76,28 @@ class SseSocketHandler extends SocketHandler {
   void shutdown() => _sseHandler.shutdown();
 }
 
+/// An implemenation of [SocketConnection] that uses WebSockets for communication
+/// by wrapping [WebSocketChannel].
 class WebSocketConnection extends SocketConnection {
   final WebSocketChannel _channel;
   WebSocketConnection(this._channel);
 
   @override
   bool get isInKeepAlivePeriod => false;
+
   @override
   StreamSink<dynamic> get sink => _channel.sink;
+
   @override
   Stream<String> get stream =>
       _channel.stream.map((dynamic o) => o?.toString());
+
   @override
   void shutdown() => _channel.sink.close();
 }
 
+/// An implemenation of [SocketHandler] that accepts WebSocket connections and
+/// wraps them in a [WebSocketConnection].
 class WebSocketSocketHandler extends SocketHandler {
   Handler _handler;
   final StreamController<WebSocketConnection> _connectionsStream =
@@ -89,8 +112,10 @@ class WebSocketSocketHandler extends SocketHandler {
   @override
   StreamQueue<WebSocketConnection> get connections =>
       _connectionsStreamQueue ??= StreamQueue(_connectionsStream.stream);
+
   @override
   FutureOr<Response> handler(Request request) => _handler(request);
+
   @override
-  void shutdown() => throw UnimplementedError();
+  void shutdown() {}
 }
