0fcf69e299e88cb3cc08470e6103775cccb7e69a
Mike Fairhurst <mfairhurst@google.com>
2020-09-21 22:34:25 -0700

    Remove unused dart:async imports
    Since Dart 2.1, Future and Stream have been exported from dart:core

--- dwds/lib/src/debugging/inspector.dart
+++ dwds/lib/src/debugging/inspector.dart
@@ -2,8 +2,6 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.import 'dart:async';
 
-import 'dart:async';
-
 import 'package:path/path.dart' as p;
 import 'package:vm_service/vm_service.dart';
 import 'package:webkit_inspection_protocol/webkit_inspection_protocol.dart';
--- dwds/lib/src/readers/asset_reader.dart
+++ dwds/lib/src/readers/asset_reader.dart
@@ -2,8 +2,6 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-import 'dart:async';
-
 /// A reader for Dart sources and related source maps.
 abstract class AssetReader {
   /// Returns the contents for a source map at the provided server path, or
