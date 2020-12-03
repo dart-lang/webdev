e5e12004778c46115aa89ec2b2d39f9ab6d34876
Gary Roumanis <grouma@google.com>
2020-12-02 16:55:37 -0800

    extend timeout

--- webdev/test/daemon/app_domain_test.dart
+++ webdev/test/daemon/app_domain_test.dart
@@ -2,7 +2,7 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-@Timeout(Duration(minutes: 2))
+@Timeout(Duration(minutes: 5))
 import 'dart:convert';
 import 'dart:io';
 
