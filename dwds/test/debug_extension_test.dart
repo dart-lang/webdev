abaa1d1e77b750cdcf1433bf0a235550c735f75c
Gary Roumanis <grouma@google.com>
2021-03-16 18:31:12 -0700

    extend

--- dwds/test/debug_extension_test.dart
+++ dwds/test/debug_extension_test.dart
@@ -7,6 +7,7 @@
 // When run locally this test may require a manifest key. This makes it easy to
 // just skip it.
 @Tags(['extension'])
+@Timeout(Duration(seconds: 60))
 @OnPlatform({
   'windows': Skip('https://github.com/dart-lang/webdev/issues/711'),
 })
