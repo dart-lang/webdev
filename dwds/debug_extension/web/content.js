9b1f4326c4cbd654d7e621a72aa768d621370de1
Gary Roumanis <grouma@google.com>
2021-04-23 15:03:52 -0700

    handle flutter stable

--- dwds/debug_extension/web/content.js
+++ dwds/debug_extension/web/content.js
@@ -1,4 +1,39 @@
-document.addEventListener('dart-app-ready', function (e) {
+var hasSent = false;
+
+function sendMessage(e) {
+    if (hasSent) return;
+    hasSent = true;
     chrome.runtime.sendMessage(e, function (response) {
     });
+}
+
+document.addEventListener('dart-app-ready', function (e) {
+    sendMessage(e);
 });
+
+var targetNode = document;
+var observerOptions = {
+    childList: true,
+    attributes: true,
+    subtree: true
+};
+
+function callback(mutationList, observer) {
+    mutationList.forEach((mutation) => {
+        if (mutation.type == 'childList') {
+            var addedNodes = mutation.addedNodes
+            for (var i = 0; i < addedNodes.length; i++) {
+                if (addedNodes[i].tagName == 'SCRIPT' &&
+                    addedNodes[i].src.includes('dart_sdk.js')) {
+                    sendMessage({});
+                }
+            }
+        }
+    });
+}
+
+// TODO(grouma) - This will become unnecessary when package:dwds version 11.0.0
+// hits Flutter stable.
+var observer = new MutationObserver(callback);
+observer.observe(targetNode, observerOptions);
+
