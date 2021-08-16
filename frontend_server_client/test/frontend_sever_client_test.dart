4aba94572bf999e3bf14880ac587e1b595b6d327
Jake Macdonald <jakemac@google.com>
2021-08-16 10:25:08 -0700

    skip failing unrelated test on windows

--- frontend_server_client/test/frontend_sever_client_test.dart
+++ frontend_server_client/test/frontend_sever_client_test.dart
@@ -117,7 +117,10 @@ String get message => p.join('hello', 'world');
 
     expect(await stdoutLines.next, p.join('goodbye', 'world'));
     expect(await process.exitCode, 0);
-  });
+  },
+      skip: Platform.isWindows
+          ? 'https://github.com/dart-lang/webdev/issues/1383'
+          : false);
 
   test('can handle compile errors and reload fixes', () async {
     var entrypoint = p.join(packageRoot, 'bin', 'main.dart');
