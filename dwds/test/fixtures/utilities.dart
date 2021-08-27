adf8fcb2331c4f17636cc8524ba9449b0604e917
Anna Gringauze <annagrin@google.com>
2021-08-26 17:45:45 -0700

    Try fix daemon not starting on windows

--- dwds/test/fixtures/utilities.dart
+++ dwds/test/fixtures/utilities.dart
@@ -16,7 +16,11 @@ Future<BuildDaemonClient> connectClient(String workingDirectory,
         List<String> options, Function(ServerLog) logHandler) =>
     BuildDaemonClient.connect(
         workingDirectory,
-        [dartPath]
+        // On Windows we need to call the snapshot directly otherwise
+        // the process will start in a disjoint cmd without access to
+        // STDIO. We also want to ensure the version of pub is consistent with
+        // the SDK that was used to launch webdev.
+        [dartPath, dartdevSnapshot]
           ..addAll(['pub', 'run', 'build_runner', 'daemon'])
           ..addAll(options),
         logHandler: logHandler);
@@ -32,6 +36,8 @@ final String _sdkDir = (() {
 
 final String dartSdkPath = _sdkDir;
 final String dartPath = p.join(_sdkDir, 'bin', 'dart');
+final String dartdevSnapshot =
+    p.join(_sdkDir, 'bin', 'snapshots', 'dartdev.dart.snapshot');
 
 /// Returns the port of the daemon asset server.
 int daemonPort(String workingDirectory) {
--- webdev/lib/src/util.dart
+++ webdev/lib/src/util.dart
@@ -20,3 +20,5 @@ final String _sdkDir = (() {
 })();
 
 final String dartPath = p.join(_sdkDir, 'bin', 'dart');
+final String dartdevSnapshot =
+    p.join(_sdkDir, 'bin', 'snapshots', 'dartdev.dart.snapshot');
