356859b9d526d420336a8041244e6888b613bf0c
Kenzie Schmoll <kenzieschmoll@google.com>
2021-11-11 12:51:09 -0800

    add todos

--- dwds/lib/src/services/chrome_proxy_service.dart
+++ dwds/lib/src/services/chrome_proxy_service.dart
@@ -1038,13 +1038,17 @@ ${globalLoadStrategy.loadModuleSnippet}("dart_sdk").developer.invokeExtension(
       throw UnimplementedError();
 
   @override
-  Future<UriList> lookupPackageUris(String isolateId, List<String> uris) =>
-      throw UnimplementedError();
+  Future<UriList> lookupPackageUris(String isolateId, List<String> uris) {
+    // TODO(https://github.com/dart-lang/webdev/issues/1446): implement.
+    throw UnimplementedError();
+  }
 
   @override
   Future<UriList> lookupResolvedPackageUris(
-          String isolateId, List<String> uris) =>
-      throw UnimplementedError();
+      String isolateId, List<String> uris) {
+    // TODO(https://github.com/dart-lang/webdev/issues/1446): implement.
+    throw UnimplementedError();
+  }
 }
 
 /// The `type`s of [ConsoleAPIEvent]s that are treated as `stderr` logs.
