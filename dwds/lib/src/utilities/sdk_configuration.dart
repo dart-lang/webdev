ede9d9c9c54a6ce00b71c7289a5a24deecd64842
Anna Gringauze <annagrin@google.com>
2022-02-17 09:50:24 -0800

    Fix more Uris on Windows

--- dwds/lib/src/utilities/sdk_configuration.dart
+++ dwds/lib/src/utilities/sdk_configuration.dart
@@ -34,16 +34,16 @@ abstract class SdkConfigurationInterface {
   Future<String> get compilerWorkerPath;
 
   Future<Uri> get sdkDirectoryUri =>
-      sdkDirectory.then((value) => value == null ? null : Uri.parse(value));
+      sdkDirectory.then((value) => value == null ? null : p.toUri(value));
 
   Future<Uri> get soundSdkSummaryUri => soundSdkSummaryPath
-      .then((value) => value == null ? null : Uri.file(value));
+      .then((value) => value == null ? null : p.toUri(value));
 
   Future<Uri> get unsoundSdkSummaryUri => unsoundSdkSummaryPath
-      .then((value) => value == null ? null : Uri.file(value));
+      .then((value) => value == null ? null : p.toUri(value));
 
   Future<Uri> get librariesUri =>
-      librariesPath.then((value) => value == null ? null : Uri.file(value));
+      librariesPath.then((value) => value == null ? null : p.toUri(value));
 
   /// Note: has to be ///file: Uri to run in an isolate.
   Future<Uri> get compilerWorkerUri => compilerWorkerPath
