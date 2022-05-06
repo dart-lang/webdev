043277e8bf50f6c16fc82b913e7efb4ad14e0700
Anna Gringauze <annagrin@google.com>
2022-05-06 15:34:57 -0700

    Format

--- dwds/test/variable_scope_test.dart
+++ dwds/test/variable_scope_test.dart
@@ -39,7 +39,8 @@ void main() {
       expect(ddcTemporaryVariableRegExp.hasMatch(r't10'), isTrue);
       expect(ddcTemporaryVariableRegExp.hasMatch(r'__t$TL'), isTrue);
       expect(ddcTemporaryVariableRegExp.hasMatch(r'__t$StringN'), isTrue);
-      expect(ddcTemporaryVariableRegExp.hasMatch(r'__t$IdentityMapOfString$T'), isTrue);
+      expect(ddcTemporaryVariableRegExp.hasMatch(r'__t$IdentityMapOfString$T'),
+          isTrue);
 
       expect(ddcTemporaryVariableRegExp.hasMatch(r't'), isFalse);
       expect(ddcTemporaryVariableRegExp.hasMatch(r't10foo'), isFalse);
