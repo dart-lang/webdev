8426dc5d5ff63efd360db7ca4bcc55a380792a9b
Elliott Brooks (she/her) <21270878+elliette@users.noreply.github.com>
2022-05-12 16:17:21 -0700

    Prepare DWDS for 2.14.2. release (#1611)

--- dwds/lib/data/build_result.g.dart
+++ dwds/lib/data/build_result.g.dart
@@ -96,7 +96,7 @@ class _$BuildResult extends BuildResult {
   final BuildStatus status;
 
   factory _$BuildResult([void Function(BuildResultBuilder) updates]) =>
-      (new BuildResultBuilder()..update(updates)).build();
+      (new BuildResultBuilder()..update(updates))._build();
 
   _$BuildResult._({this.status}) : super._() {
     BuiltValueNullFieldError.checkNotNull(status, 'BuildResult', 'status');
@@ -157,7 +157,9 @@ class BuildResultBuilder implements Builder<BuildResult, BuildResultBuilder> {
   }
 
   @override
-  _$BuildResult build() {
+  BuildResult build() => _build();
+
+  _$BuildResult _build() {
     final _$result = _$v ??
         new _$BuildResult._(
             status: BuiltValueNullFieldError.checkNotNull(
@@ -167,4 +169,4 @@ class BuildResultBuilder implements Builder<BuildResult, BuildResultBuilder> {
   }
 }
 
-// ignore_for_file: always_put_control_body_on_new_line,always_specify_types,annotate_overrides,avoid_annotating_with_dynamic,avoid_as,avoid_catches_without_on_clauses,avoid_returning_this,deprecated_member_use_from_same_package,lines_longer_than_80_chars,omit_local_variable_types,prefer_expression_function_bodies,sort_constructors_first,test_types_in_equals,unnecessary_const,unnecessary_new
+// ignore_for_file: always_put_control_body_on_new_line,always_specify_types,annotate_overrides,avoid_annotating_with_dynamic,avoid_as,avoid_catches_without_on_clauses,avoid_returning_this,deprecated_member_use_from_same_package,lines_longer_than_80_chars,no_leading_underscores_for_local_identifiers,omit_local_variable_types,prefer_expression_function_bodies,sort_constructors_first,test_types_in_equals,unnecessary_const,unnecessary_new
--- dwds/lib/data/connect_request.g.dart
+++ dwds/lib/data/connect_request.g.dart
@@ -75,7 +75,7 @@ class _$ConnectRequest extends ConnectRequest {
   final String entrypointPath;
 
   factory _$ConnectRequest([void Function(ConnectRequestBuilder) updates]) =>
-      (new ConnectRequestBuilder()..update(updates)).build();
+      (new ConnectRequestBuilder()..update(updates))._build();
 
   _$ConnectRequest._({this.appId, this.instanceId, this.entrypointPath})
       : super._() {
@@ -161,7 +161,9 @@ class ConnectRequestBuilder
   }
 
   @override
-  _$ConnectRequest build() {
+  ConnectRequest build() => _build();
+
+  _$ConnectRequest _build() {
     final _$result = _$v ??
         new _$ConnectRequest._(
             appId: BuiltValueNullFieldError.checkNotNull(
@@ -175,4 +177,4 @@ class ConnectRequestBuilder
   }
 }
 
-// ignore_for_file: always_put_control_body_on_new_line,always_specify_types,annotate_overrides,avoid_annotating_with_dynamic,avoid_as,avoid_catches_without_on_clauses,avoid_returning_this,deprecated_member_use_from_same_package,lines_longer_than_80_chars,omit_local_variable_types,prefer_expression_function_bodies,sort_constructors_first,test_types_in_equals,unnecessary_const,unnecessary_new
+// ignore_for_file: always_put_control_body_on_new_line,always_specify_types,annotate_overrides,avoid_annotating_with_dynamic,avoid_as,avoid_catches_without_on_clauses,avoid_returning_this,deprecated_member_use_from_same_package,lines_longer_than_80_chars,no_leading_underscores_for_local_identifiers,omit_local_variable_types,prefer_expression_function_bodies,sort_constructors_first,test_types_in_equals,unnecessary_const,unnecessary_new
