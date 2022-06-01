00c5df2947b8a578f7bfc064810404028fcb6ab2
Elliott Brooks <21270878+elliette@users.noreply.github.com>
2022-06-01 15:00:55 -0700

    Format

--- dwds/lib/src/services/chrome_proxy_service.dart
+++ dwds/lib/src/services/chrome_proxy_service.dart
@@ -691,8 +691,8 @@ ${globalLoadStrategy.loadModuleSnippet}("dart_sdk").developer.invokeExtension(
   // Note: Ignore the optional local parameter, it is there to keep the method
   // signature consistent with the VM service interface.
   @override
-  Future<UriList> lookupResolvedPackageUris(
-      String isolateId, List<String> uris, {bool local}) async {
+  Future<UriList> lookupResolvedPackageUris(String isolateId, List<String> uris,
+      {bool local}) async {
     await isInitialized;
     return UriList(uris: uris.map(DartUri.toResolvedUri).toList());
   }
