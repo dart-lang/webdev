f97242b0a275ef8904b3dd2a5ed6bf4ed65133d5
Elliott Brooks <21270878+elliette@users.noreply.github.com>
2022-06-02 09:55:51 -0700

    Prep to publish DWDS v.14.0.3

--- dwds/lib/data/build_result.g.dart
+++ dwds/lib/data/build_result.g.dart
@@ -98,7 +98,7 @@ class _$BuildResult extends BuildResult {
       (new BuildResultBuilder()..update(updates))._build();
 
   _$BuildResult._({required this.status}) : super._() {
-    BuiltValueNullFieldError.checkNotNull(status, 'BuildResult', 'status');
+    BuiltValueNullFieldError.checkNotNull(status, r'BuildResult', 'status');
   }
 
   @override
@@ -121,7 +121,7 @@ class _$BuildResult extends BuildResult {
 
   @override
   String toString() {
-    return (newBuiltValueToStringHelper('BuildResult')..add('status', status))
+    return (newBuiltValueToStringHelper(r'BuildResult')..add('status', status))
         .toString();
   }
 }
@@ -162,10 +162,10 @@ class BuildResultBuilder implements Builder<BuildResult, BuildResultBuilder> {
     final _$result = _$v ??
         new _$BuildResult._(
             status: BuiltValueNullFieldError.checkNotNull(
-                status, 'BuildResult', 'status'));
+                status, r'BuildResult', 'status'));
     replace(_$result);
     return _$result;
   }
 }
 
-// ignore_for_file: always_put_control_body_on_new_line,always_specify_types,annotate_overrides,avoid_annotating_with_dynamic,avoid_as,avoid_catches_without_on_clauses,avoid_returning_this,deprecated_member_use_from_same_package,lines_longer_than_80_chars,no_leading_underscores_for_local_identifiers,omit_local_variable_types,prefer_expression_function_bodies,sort_constructors_first,test_types_in_equals,unnecessary_const,unnecessary_new
+// ignore_for_file: always_put_control_body_on_new_line,always_specify_types,annotate_overrides,avoid_annotating_with_dynamic,avoid_as,avoid_catches_without_on_clauses,avoid_returning_this,deprecated_member_use_from_same_package,lines_longer_than_80_chars,no_leading_underscores_for_local_identifiers,omit_local_variable_types,prefer_expression_function_bodies,sort_constructors_first,test_types_in_equals,unnecessary_const,unnecessary_new,unnecessary_lambdas
