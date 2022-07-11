0beca2cc6c095ba68a9b9f8ef86987f264c83c70
Elliott Brooks <21270878+elliette@users.noreply.github.com>
2022-07-11 15:49:20 -0700

    Dart format

--- dwds/test/fixtures/debugger_data.dart
+++ dwds/test/fixtures/debugger_data.dart
@@ -2,8 +2,6 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-
-
 /// Contains hard-coded test data usable for tests.
 import 'package:webkit_inspection_protocol/webkit_inspection_protocol.dart';
 
