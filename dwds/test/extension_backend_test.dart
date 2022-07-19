9214e05d1a2371fc0ee33ce8dd21bce58ac9fc8c
Elliott Brooks (she/her) <21270878+elliette@users.noreply.github.com>
2022-07-19 16:34:19 -0700

    Convert more test files to null-safety (#1683)

--- dwds/test/extension_backend_test.dart
+++ dwds/test/extension_backend_test.dart
@@ -2,8 +2,6 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-// @dart = 2.9
-
 import 'dart:async';
 
 import 'package:async/src/stream_queue.dart';
@@ -31,7 +29,7 @@ class MockSocketHandler implements SocketHandler {
 }
 
 void main() {
-  ExtensionBackend extensionBackend;
+  late ExtensionBackend extensionBackend;
 
   setUpAll(() async {
     extensionBackend =
