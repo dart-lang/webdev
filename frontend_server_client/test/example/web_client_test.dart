c97f5a2a078249d703bd878f4579bc742c674184
Jake Macdonald <jakemac@google.com>
2022-07-21 11:57:12 -0700

    skip windows tests

--- frontend_server_client/test/example/web_client_test.dart
+++ frontend_server_client/test/example/web_client_test.dart
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+// TODO: The examples don't work on windows
+@TestOn('!windows')
 import 'dart:io';
 
 import 'package:test/test.dart';
