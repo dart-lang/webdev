052131842d0ffaddacf71d00d21e104e71281a13
Jake Macdonald <jakemac@google.com>
2022-07-22 08:11:40 -0700

    make the examples clean up the output dir

--- frontend_server_client/example/web_client.dart
+++ frontend_server_client/example/web_client.dart
@@ -14,106 +14,112 @@ import 'package:shelf_packages_handler/shelf_packages_handler.dart';
 import 'package:shelf_static/shelf_static.dart';
 
 void main(List<String> args) async {
-  watch.start();
-  if (args.isNotEmpty) {
-    throw ArgumentError('No command line args are supported');
-  }
-
-  _print('compiling the dart sdk');
-  var sdkCompileResult = await Process.run(Platform.resolvedExecutable, [
-    p.join(sdkDir, 'bin', 'snapshots', 'dartdevc.dart.snapshot'),
-    '--multi-root-scheme=org-dartlang-sdk',
-    '--modules=amd',
-    '--module-name=dart_sdk',
-    '--sound-null-safety',
-    '-o',
-    dartSdkJs,
-    p.url.join(sdkDir, sdkKernelPath),
-  ]);
-  if (sdkCompileResult.exitCode != 0) {
-    _print('Failed to compile the dart sdk to JS:\n'
-        '${sdkCompileResult.stdout}\n'
-        '${sdkCompileResult.stderr}');
-    exit(sdkCompileResult.exitCode);
-  }
-
-  _print('starting frontend server');
-  var client = await DartDevcFrontendServerClient.start(
-      'org-dartlang-root:///$app', outputDill,
-      fileSystemRoots: [p.current],
-      fileSystemScheme: 'org-dartlang-root',
-      platformKernel: p.toUri(sdkKernelPath).toString(),
-      verbose: true);
-
-  _print('compiling $app');
-  await client.compile([]);
-  client.accept();
-  _print('done compiling $app');
-
-  _print('starting shelf server');
-  var cascade = Cascade()
-      .add(_clientHandler(client))
-      .add(createStaticHandler(p.current))
-      .add(createFileHandler(dartSdkJs, url: 'example/app/dart_sdk.js'))
-      .add(createFileHandler(
-          p.join(sdkDir, 'lib', 'dev_compiler', 'web',
-              'dart_stack_trace_mapper.js'),
-          url: 'example/app/dart_stack_trace_mapper.js'))
-      .add(createFileHandler(
-          p.join(sdkDir, 'lib', 'dev_compiler', 'kernel', 'amd', 'require.js'),
-          url: 'example/app/require.js'))
-      .add(packagesDirHandler());
-  final server = await shelf_io.serve(cascade.handler, 'localhost', 8080);
-  _print('server ready');
-
-  // The file we will be editing in the repl
-  var appFile = File(app);
-  var originalContent = await appFile.readAsString();
-  var appLines = const LineSplitter().convert(originalContent);
-  var getterText = 'String get message =>';
-  var messageLine = appLines.indexWhere((line) => line.startsWith(getterText));
-
-  var stdinQueue = StreamQueue(
-      stdin.transform(utf8.decoder).transform(const LineSplitter()));
-  _prompt();
-  while (await stdinQueue.hasNext) {
-    var newMessage = await stdinQueue.next;
-    if (newMessage == 'quit') {
-      await server.close();
-      await stdinQueue.cancel();
-      break;
-    } else if (newMessage == 'reset') {
-      print('resetting');
-      client.reset();
-      _print('restoring $app');
-      await appFile.writeAsString(originalContent);
-    } else {
-      _print('editing $app');
-      appLines[messageLine] = '$getterText "$newMessage";';
-      var newContent = appLines.join('\n');
-      await appFile.writeAsString(newContent);
-
-      _print('recompiling $app with edits');
-      var result =
-          await client.compile([Uri.parse('org-dartlang-root:///$app')]);
-      if (result.errorCount > 0) {
-        print('Compile errors: \n${result.compilerOutputLines.join('\n')}');
-        await client.reject();
-      } else {
-        _print('Recompile succeeded for $app');
-        client.accept();
-        // TODO: support hot restart
-        print('reload app to see the new message');
-      }
+  try {
+    watch.start();
+    if (args.isNotEmpty) {
+      throw ArgumentError('No command line args are supported');
     }
 
-    _prompt();
-  }
+    _print('compiling the dart sdk');
+    var sdkCompileResult = await Process.run(Platform.resolvedExecutable, [
+      p.join(sdkDir, 'bin', 'snapshots', 'dartdevc.dart.snapshot'),
+      '--multi-root-scheme=org-dartlang-sdk',
+      '--modules=amd',
+      '--module-name=dart_sdk',
+      '--sound-null-safety',
+      '-o',
+      dartSdkJs,
+      p.url.join(sdkDir, sdkKernelPath),
+    ]);
+    if (sdkCompileResult.exitCode != 0) {
+      _print('Failed to compile the dart sdk to JS:\n'
+          '${sdkCompileResult.stdout}\n'
+          '${sdkCompileResult.stderr}');
+      exit(sdkCompileResult.exitCode);
+    }
 
-  _print('restoring $app');
-  await appFile.writeAsString(originalContent);
-  _print('exiting');
-  await client.shutdown();
+    _print('starting frontend server');
+    var client = await DartDevcFrontendServerClient.start(
+        'org-dartlang-root:///$app', outputDill,
+        fileSystemRoots: [p.current],
+        fileSystemScheme: 'org-dartlang-root',
+        platformKernel: p.toUri(sdkKernelPath).toString(),
+        verbose: true);
+
+    _print('compiling $app');
+    await client.compile([]);
+    client.accept();
+    _print('done compiling $app');
+
+    _print('starting shelf server');
+    var cascade = Cascade()
+        .add(_clientHandler(client))
+        .add(createStaticHandler(p.current))
+        .add(createFileHandler(dartSdkJs, url: 'example/app/dart_sdk.js'))
+        .add(createFileHandler(
+            p.join(sdkDir, 'lib', 'dev_compiler', 'web',
+                'dart_stack_trace_mapper.js'),
+            url: 'example/app/dart_stack_trace_mapper.js'))
+        .add(createFileHandler(
+            p.join(
+                sdkDir, 'lib', 'dev_compiler', 'kernel', 'amd', 'require.js'),
+            url: 'example/app/require.js'))
+        .add(packagesDirHandler());
+    final server = await shelf_io.serve(cascade.handler, 'localhost', 8080);
+    _print('server ready');
+
+    // The file we will be editing in the repl
+    var appFile = File(app);
+    var originalContent = await appFile.readAsString();
+    var appLines = const LineSplitter().convert(originalContent);
+    var getterText = 'String get message =>';
+    var messageLine =
+        appLines.indexWhere((line) => line.startsWith(getterText));
+
+    var stdinQueue = StreamQueue(
+        stdin.transform(utf8.decoder).transform(const LineSplitter()));
+    _prompt();
+    while (await stdinQueue.hasNext) {
+      var newMessage = await stdinQueue.next;
+      if (newMessage == 'quit') {
+        await server.close();
+        await stdinQueue.cancel();
+        break;
+      } else if (newMessage == 'reset') {
+        print('resetting');
+        client.reset();
+        _print('restoring $app');
+        await appFile.writeAsString(originalContent);
+      } else {
+        _print('editing $app');
+        appLines[messageLine] = '$getterText "$newMessage";';
+        var newContent = appLines.join('\n');
+        await appFile.writeAsString(newContent);
+
+        _print('recompiling $app with edits');
+        var result =
+            await client.compile([Uri.parse('org-dartlang-root:///$app')]);
+        if (result.errorCount > 0) {
+          print('Compile errors: \n${result.compilerOutputLines.join('\n')}');
+          await client.reject();
+        } else {
+          _print('Recompile succeeded for $app');
+          client.accept();
+          // TODO: support hot restart
+          print('reload app to see the new message');
+        }
+      }
+
+      _prompt();
+    }
+
+    _print('restoring $app');
+    await appFile.writeAsString(originalContent);
+    _print('exiting');
+    await client.shutdown();
+  } finally {
+    Directory(p.join('dart_tool', 'out')).deleteSync(recursive: true);
+  }
 }
 
 Handler _clientHandler(DartDevcFrontendServerClient client) {
