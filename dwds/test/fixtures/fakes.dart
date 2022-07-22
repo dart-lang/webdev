dd560d08367e9137a6fa9e3535f47534d2d2620f
Elliott Brooks <21270878+elliette@users.noreply.github.com>
2022-07-21 18:02:30 -0700

    Migrated the FakeWebkitDebugger

--- dwds/test/fixtures/fakes.dart
+++ dwds/test/fixtures/fakes.dart
@@ -2,22 +2,19 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-// @dart = 2.9
+
 
 import 'dart:async';
 
-import 'package:dwds/src/debugging/classes.dart';
+import 'package:dwds/expression_compiler.dart';
 import 'package:dwds/src/debugging/execution_context.dart';
-import 'package:dwds/src/debugging/inspector.dart';
-import 'package:dwds/src/debugging/instance.dart';
-import 'package:dwds/src/debugging/libraries.dart';
+import 'package:dwds/src/debugging/metadata/provider.dart';
 import 'package:dwds/src/debugging/modules.dart';
 import 'package:dwds/src/debugging/remote_debugger.dart';
 import 'package:dwds/src/debugging/webkit_debugger.dart';
 import 'package:dwds/src/handlers/socket_connections.dart';
 import 'package:dwds/src/loaders/require.dart';
 import 'package:dwds/src/loaders/strategy.dart';
-import 'package:vm_service/vm_service.dart';
 
 /// A library of fake/stub implementations of our classes and their supporting
 /// classes (e.g. WipConnection) for unit testing.
@@ -69,124 +66,6 @@ class FakeModules implements Modules {
   Future<String> moduleForlibrary(String libraryUri) {
     throw UnimplementedError();
   }
-}
-
-class FakeWebkitDebugger implements WebkitDebugger {
-  final Map<String, WipScript> _scripts;
-  @override
-  Future disable() => null;
-
-  @override
-  Future enable() => null;
-
-  FakeWebkitDebugger({Map<String, WipScript> scripts}) : _scripts = scripts {
-    globalLoadStrategy = RequireStrategy(
-        ReloadConfiguration.none,
-        (_) async => {},
-        (_) async => {},
-        (_, __) async => null,
-        (_, __) async => null,
-        (_, __) async => null,
-        null,
-        (_) async => null,
-        null);
-  }
-
-  @override
-  Stream<T> eventStream<T>(String method, WipEventTransformer<T> transformer) =>
-      null;
-
-  @override
-  Future<String> getScriptSource(String scriptId) => null;
-
-  Stream<WipDomain> get onClosed => null;
-
-  @override
-  Stream<GlobalObjectClearedEvent> get onGlobalObjectCleared => null;
-
-  @override
-  Stream<DebuggerPausedEvent> onPaused;
-
-  @override
-  Stream<DebuggerResumedEvent> get onResumed => null;
-
-  @override
-  Stream<ScriptParsedEvent> get onScriptParsed => null;
-
-  @override
-  Stream<TargetCrashedEvent> get onTargetCrashed => null;
-
-  @override
-  Future<WipResponse> pause() => null;
-
-  @override
-  Future<WipResponse> resume() => null;
-
-  @override
-  Map<String, WipScript> get scripts => _scripts;
-
-  List<WipResponse> results = variables1;
-  int resultsReturned = 0;
-
-  @override
-  Future<WipResponse> sendCommand(
-    String method, {
-    Map<String, dynamic> params,
-  }) async {
-    // Force the results that we expect for looking up the variables.
-    if (method == 'Runtime.getProperties') {
-      return results[resultsReturned++];
-    }
-    return null;
-  }
-
-  @override
-  Future<WipResponse> setPauseOnExceptions(PauseState state) => null;
-
-  @override
-  Future<WipResponse> removeBreakpoint(String breakpointId) => null;
-
-  @override
-  Future<WipResponse> stepInto({Map<String, dynamic> params}) => null;
-
-  @override
-  Future<WipResponse> stepOut() => null;
-
-  @override
-  Future<WipResponse> stepOver({Map<String, dynamic> params}) => null;
-
-  @override
-  Stream<ConsoleAPIEvent> get onConsoleAPICalled => null;
-
-  @override
-  Stream<ExceptionThrownEvent> get onExceptionThrown => null;
-
-  @override
-  void close() {}
-
-  @override
-  Stream<WipConnection> get onClose => null;
-
-  @override
-  Future<RemoteObject> evaluate(String expression,
-          {bool returnByValue, int contextId}) =>
-      null;
-
-  @override
-  Future<RemoteObject> evaluateOnCallFrame(
-      String callFrameId, String expression) async {
-    return RemoteObject(<String, dynamic>{});
-  }
-
-  @override
-  Future<List<WipBreakLocation>> getPossibleBreakpoints(WipLocation start) =>
-      null;
-
-  @override
-  Future<WipResponse> enablePage() => null;
-
-  @override
-  Future<WipResponse> pageReload() => null;
 }
 
 /// Fake execution context that is needed for id only
--- dwds/test/fixtures/migrated_fakes.dart
+++ dwds/test/fixtures/migrated_fakes.dart
@@ -14,6 +14,12 @@ import 'package:dwds/src/debugging/inspector.dart';
 import 'package:dwds/src/debugging/instance.dart';
 import 'package:dwds/src/debugging/libraries.dart';
 import 'package:vm_service/vm_service.dart';
+import 'package:dwds/src/debugging/remote_debugger.dart';
+import 'package:dwds/src/debugging/webkit_debugger.dart';
+import 'package:dwds/src/loaders/require.dart';
+import 'package:webkit_inspection_protocol/webkit_inspection_protocol.dart';
+
+import 'debugger_data.dart';
 
 
 class FakeStrategy implements LoadStrategy {
@@ -175,4 +181,125 @@ class FakeInspector implements AppInspector {
         name: fakeIsolate.name,
         isSystemIsolate: fakeIsolate.isSystemIsolate,
       );
+}
+
+class FakeWebkitDebugger implements WebkitDebugger {
+  final Map<String, WipScript>? _scripts;
+  @override
+  Future disable() => Future.value(null);
+
+  @override
+  Future enable() => Future.value(null);
+
+  FakeWebkitDebugger({Map<String, WipScript>? scripts}) : _scripts = scripts {
+    globalLoadStrategy = RequireStrategy(
+      ReloadConfiguration.none,
+      (_) async => {},
+      (_) async => {},
+      (_, __) async => null,
+      (MetadataProvider _, String __) async => Future.value(''),
+      (MetadataProvider _, String __) async => Future.value(''),
+      (String _) => '',
+      (MetadataProvider _) async => Future.value(<String, ModuleInfo>{}),
+      FakeAssetReader(),
+    );
+  }
+
+  @override
+  Stream<T> eventStream<T>(String method, WipEventTransformer<T> transformer) =>
+      Stream.empty();
+
+  @override
+  Future<String> getScriptSource(String scriptId) => Future.value('');
+
+  Stream<WipDomain>? get onClosed => null;
+
+  @override
+  Stream<GlobalObjectClearedEvent> get onGlobalObjectCleared => Stream.empty();
+
+  @override
+  late Stream<DebuggerPausedEvent> onPaused;
+
+  @override
+  Stream<DebuggerResumedEvent> get onResumed => Stream.empty();
+
+  @override
+  Stream<ScriptParsedEvent> get onScriptParsed => Stream.empty();
+
+  @override
+  Stream<TargetCrashedEvent> get onTargetCrashed => Stream.empty();
+
+  @override
+  Future<WipResponse> pause() => Future.value(WipResponse({}));
+
+  @override
+  Future<WipResponse> resume() => Future.value(WipResponse({}));
+
+  @override
+  Map<String, WipScript> get scripts => _scripts!;
+
+  List<WipResponse> results = variables1;
+  int resultsReturned = 0;
+
+  @override
+  Future<WipResponse> sendCommand(String command,
+      {Map<String, dynamic>? params}) async {
+    // Force the results that we expect for looking up the variables.
+    if (command == 'Runtime.getProperties') {
+      return Future.value(results[resultsReturned++]);
+    }
+    return Future.value(WipResponse({}));
+  }
+
+  @override
+  Future<WipResponse> setPauseOnExceptions(PauseState state) =>
+      Future.value(WipResponse({}));
+
+  @override
+  Future<WipResponse> removeBreakpoint(String breakpointId) =>
+      Future.value(WipResponse({}));
+
+  @override
+  Future<WipResponse> stepInto({Map<String, dynamic>? params}) =>
+      Future.value(WipResponse({}));
+
+  @override
+  Future<WipResponse> stepOut() => Future.value(WipResponse({}));
+
+  @override
+  Future<WipResponse> stepOver({Map<String, dynamic>? params}) =>
+      Future.value(WipResponse({}));
+
+  @override
+  Stream<ConsoleAPIEvent> get onConsoleAPICalled => Stream.empty();
+
+  @override
+  Stream<ExceptionThrownEvent> get onExceptionThrown => Stream.empty();
+
+  @override
+  void close() {}
+
+  @override
+  Stream<WipConnection> get onClose => Stream.empty();
+
+  @override
+  Future<RemoteObject> evaluate(String expression,
+          {bool? returnByValue, int? contextId}) =>
+      Future.value(RemoteObject({}));
+
+  @override
+  Future<RemoteObject> evaluateOnCallFrame(
+      String callFrameId, String expression) async {
+    return RemoteObject(<String, dynamic>{});
+  }
+
+  @override
+  Future<List<WipBreakLocation>> getPossibleBreakpoints(WipLocation start) =>
+      Future.value([]);
+
+  @override
+  Future<WipResponse> enablePage() => Future.value(WipResponse({}));
+
+  @override
+  Future<WipResponse> pageReload() => Future.value(WipResponse({}));
 }
