95c4756a823d5761db09455fca24cf6207bf66b7
Elliott Brooks <21270878+elliette@users.noreply.github.com>
2022-08-08 12:16:03 -0700

    Format

--- dwds/test/debug_extension_test.dart
+++ dwds/test/debug_extension_test.dart
@@ -2,8 +2,6 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-
-
 // When run locally this test may require a manifest key. This makes it easy to
 // just skip it.
 @Tags(['extension'])
--- dwds/test/readers/proxy_server_asset_reader_test.dart
+++ dwds/test/readers/proxy_server_asset_reader_test.dart
@@ -2,8 +2,6 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-
-
 import 'package:dwds/src/readers/proxy_server_asset_reader.dart';
 import 'package:test/test.dart';
 
