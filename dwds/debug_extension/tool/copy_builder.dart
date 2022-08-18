6bc0ddcc9e6927a1bc52941b7c7dc49408188c86
Elliott Brooks <21270878+elliette@users.noreply.github.com>
2022-08-17 17:57:23 -0700

    Better way of handling extension key

--- dwds/debug_extension/tool/copy_builder.dart
+++ dwds/debug_extension/tool/copy_builder.dart
@@ -2,6 +2,8 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+import 'dart:io';
+
 import 'package:build/build.dart';
 
 /// Factory for the build script.
@@ -14,14 +16,14 @@ class _CopyBuilder extends Builder {
         "web/{{}}.png": ["build/web_prod/{{}}.png"],
         "web/{{}}.html": ["build/web_prod/{{}}.html"],
         "web/{{}}.css": ["build/web_prod/{{}}.css"],
-        "web/manifest_prod.json": ["build/web_prod/manifest.json"],
+        "web/manifest.json": ["build/web_prod/manifest.json"],
         "web/panel.js": ["build/web_prod/panel.js"],
         "web/detector.js": ["build/web_prod/detector.js"],
         "web/devtools.js": ["build/web_prod/devtools.js"],
       };
 
   @override
-  void build(BuildStep buildStep) {
+  void build(BuildStep buildStep) async {
     final inputAsset = buildStep.inputId;
     final allowedOutputs = buildStep.allowedOutputs;
 
@@ -30,15 +32,50 @@ class _CopyBuilder extends Builder {
     }
 
     final outputAsset = allowedOutputs.first;
-    _copyBinaryFile(buildStep,
+    await _copyBinaryFile(buildStep,
         inputAsset: inputAsset, outputAsset: outputAsset);
+
+    if (outputAsset.path.endsWith('manifest.json')) {
+      await _maybeAddExtensionKey(outputAsset.path);
+    }
   }
 
-  void _copyBinaryFile(
+  Future<void> _copyBinaryFile(
     BuildStep buildStep, {
     required AssetId inputAsset,
     required AssetId outputAsset,
   }) {
-    buildStep.writeAsBytes(outputAsset, buildStep.readAsBytes(inputAsset));
+    return buildStep.writeAsBytes(outputAsset, buildStep.readAsBytes(inputAsset));
+  }
+
+  Future<void> _maybeAddExtensionKey(String manifestJsonPath) async {
+    final manifestJson = File(manifestJsonPath);
+    final extensionKey = File('extension_key.txt');
+    if (await extensionKey.exists()) {
+      return _addExtensionKey(manifestJson, extensionKey);
+    }
+    return null;
+  }
+
+  Future<void> _addExtensionKey(File manifestJson, File extensionKey) async {
+    final lines = manifestJson.readAsLinesSync();
+    final newLines = <String>[];
+
+    for (final line in lines) {
+      newLines.add(line);
+      if (line.trim().startsWith('"name":')) {
+        final keyValue = await extensionKey.readAsString();
+        newLines.add('    "key": "$keyValue",');
+      }
+    }
+
+    final content = newLines.joinWithNewLine();
+    return manifestJson.writeAsStringSync(content);
+  }
+}
+
+extension JoinExtension on List<String> {
+  String joinWithNewLine() {
+    return '${join('\n')}\n';
   }
 }
