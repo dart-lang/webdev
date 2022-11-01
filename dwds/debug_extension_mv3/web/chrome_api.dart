daa154d1410408fcc00f7286a0fd19a74dc4d348
Elliott Brooks (she/her) <21270878+elliette@users.noreply.github.com>
2022-11-01 10:14:54 -0700

    Reset MV3 extension to nice starting point (#1773)

--- dwds/debug_extension_mv3/web/chrome_api.dart
+++ dwds/debug_extension_mv3/web/chrome_api.dart
@@ -11,16 +11,17 @@ external Chrome get chrome;
 @anonymous
 class Chrome {
   external Action get action;
-  external Debugger get debugger;
   external Runtime get runtime;
-  external Scripting get scripting;
-  external Tabs get tabs;
 }
+
+/// chrome.action APIs
+/// https://developer.chrome.com/docs/extensions/reference/action
 
 @JS()
 @anonymous
 class Action {
-  // https://developer.chrome.com/docs/extensions/reference/action/#event-onClicked
+  external void setIcon(IconInfo iconInfo, Function? callback);
+
   external OnClickedHandler get onClicked;
 }
 
@@ -32,27 +33,20 @@ class OnClickedHandler {
 
 @JS()
 @anonymous
-class Debugger {
-  // https://developer.chrome.com/docs/extensions/reference/debugger/#method-attach
-  external void attach(
-      Debuggee target, String requiredVersion, Function? callback);
-
-  // https://developer.chrome.com/docs/extensions/reference/debugger/#method-sendCommand
-  external void sendCommand(Debuggee target, String method,
-      Object? commandParams, Function? callback);
+class IconInfo {
+  external String get path;
+  external factory IconInfo({String path});
 }
+
+/// chrome.runtime APIs:
+/// https://developer.chrome.com/docs/extensions/reference/runtime
 
 @JS()
 @anonymous
 class Runtime {
-  // https://developer.chrome.com/docs/extensions/reference/runtime/#method-sendMessage
   external void sendMessage(
       String? id, Object? message, Object? options, Function? callback);
 
-  // https://developer.chrome.com/docs/extensions/reference/runtime/#method-getURL
-  external String getURL(String path);
-
-  // https://developer.chrome.com/docs/extensions/reference/runtime/#event-onMessage
   external OnMessageHandler get onMessage;
 }
 
@@ -61,32 +55,6 @@ class Runtime {
 class OnMessageHandler {
   external void addListener(
       void Function(dynamic, MessageSender, Function) callback);
-}
-
-@JS()
-@anonymous
-class Scripting {
-  // https://developer.chrome.com/docs/extensions/reference/scripting/#method-executeScript
-  external executeScript(InjectDetails details, Function? callback);
-}
-
-@JS()
-@anonymous
-class Tabs {
-  // https://developer.chrome.com/docs/extensions/reference/tabs/#method-query
-  external Object query(QueryInfo queryInfo);
-
-  // https://developer.chrome.com/docs/extensions/reference/tabs/#method-create
-  external Object create(TabInfo tabInfo);
-}
-
-@JS()
-@anonymous
-class Debuggee {
-  external int get tabId;
-  external String get extensionId;
-  external String get targetId;
-  external factory Debuggee({int tabId, String? extensionId, String? targetId});
 }
 
 @JS()
@@ -100,39 +68,7 @@ class MessageSender {
 
 @JS()
 @anonymous
-class TabInfo {
-  external bool? get active;
-  external bool? get pinned;
-  external String? get url;
-  external factory TabInfo({bool? active, bool? pinned, String? url});
-}
-
-@JS()
-@anonymous
-class QueryInfo {
-  external bool get active;
-  external bool get currentWindow;
-  external factory QueryInfo({bool? active, bool? currentWindow});
-}
-
-@JS()
-@anonymous
 class Tab {
   external int get id;
   external String get url;
-}
-
-@JS()
-@anonymous
-class InjectDetails {
-  external Target get target;
-  external List<String>? get files;
-  external factory InjectDetails({Target target, List<String> files});
-}
-
-@JS()
-@anonymous
-class Target {
-  external int get tabId;
-  external factory Target({int tabId});
 }
