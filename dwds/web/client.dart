97e8bf5563607d392948ad611cf6e0ec44873b58
Elliott Brooks <21270878+elliette@users.noreply.github.com>
2022-12-01 13:18:24 -0800

    Send isFlutterApp to extension

--- dwds/web/client.dart
+++ dwds/web/client.dart
@@ -181,7 +181,8 @@ Future<void>? main() {
       ..appOrigin = window.location.origin
       ..appUrl = window.location.href
       ..extensionUrl = windowContext['\$dartExtensionUri']
-      ..isInternalBuild = windowContext['\$isInternalBuild'])));
+      ..isInternalBuild = windowContext['\$isInternalBuild']
+      ..isFlutterApp = windowContext['\$isFlutterApp'])));
 
     dispatchEvent(CustomEvent('dart-app-ready', detail: debugInfoJson));
   }, (error, stackTrace) {
