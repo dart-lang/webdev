91b8a193a28538689f125dc669f15e02a1797ab2
Elliott Brooks (she/her) <21270878+elliette@users.noreply.github.com>
2022-12-01 11:52:10 -0800

    Fix global variable `isInternalBuild` in injected client (#1805)

--- dwds/lib/dart_web_debug_service.dart
+++ dwds/lib/dart_web_debug_service.dart
@@ -128,6 +128,7 @@ class Dwds {
       extensionUri: extensionUri,
       enableDevtoolsLaunch: enableDevtoolsLaunch,
       emitDebugEvents: emitDebugEvents,
+      isInternalBuild: isInternalBuild,
     );
 
     final devHandler = DevHandler(
--- dwds/web/client.dart
+++ dwds/web/client.dart
@@ -181,7 +181,7 @@ Future<void>? main() {
       ..appOrigin = window.location.origin
       ..appUrl = window.location.href
       ..extensionUrl = windowContext['\$dartExtensionUri']
-      ..isInternalBuild = windowContext['\$isInternalDartBuild'])));
+      ..isInternalBuild = windowContext['\$isInternalBuild'])));
 
     dispatchEvent(CustomEvent('dart-app-ready', detail: debugInfoJson));
   }, (error, stackTrace) {
@@ -275,7 +275,7 @@ external set emitDebugEvent(void Function(String, String) func);
 @JS(r'$emitRegisterEvent')
 external set emitRegisterEvent(void Function(String) func);
 
-@JS(r'$isInternalDartBuild')
-external bool get isInternalDartBuild;
+@JS(r'$isInternalBuild')
+external bool get isInternalBuild;
 
 bool get _isChromium => window.navigator.vendor.contains('Google');
