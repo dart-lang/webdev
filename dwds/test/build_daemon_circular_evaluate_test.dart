80f0130fc2f3ebedc7faab7ba6e511b382b0e214
Elliott Brooks <21270878+elliette@users.noreply.github.com>
2022-12-09 09:06:45 -0800

    Fix first test

--- dwds/test/build_daemon_circular_evaluate_test.dart
+++ dwds/test/build_daemon_circular_evaluate_test.dart
@@ -3,7 +3,9 @@
 // BSD-style license that can be found in the LICENSE file.
 
 @TestOn('vm')
+import 'dart:io';
 
+import 'package:pub_semver/pub_semver.dart';
 import 'package:test/test.dart';
 
 import 'fixtures/context.dart';
@@ -12,8 +14,12 @@ import 'evaluate_circular_common.dart';
 void main() async {
   // Enable verbose logging for debugging.
   final debug = false;
+  final sdkVersion = Version.parse(Platform.version.split(' ')[0]);
+  final nullSafetyValues = (sdkVersion.major >= 3)
+      ? [NullSafety.sound]
+      : [NullSafety.sound, NullSafety.weak];
 
-  for (var nullSafety in NullSafety.values) {
+  for (var nullSafety in nullSafetyValues) {
     group('${nullSafety.name} null safety |', () {
       testAll(
         compilationMode: CompilationMode.buildDaemon,
