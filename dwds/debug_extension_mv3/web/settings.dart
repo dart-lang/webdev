1d1c98fa17fc609739dad32d811c5ef7523cb48f
Elliott Brooks (she/her) <21270878+elliette@users.noreply.github.com>
2022-12-14 09:19:29 -0800

    Settings page, Dart Debugger panel, and Flutter Inspector panel match DevTools styles (#1815)

--- dwds/debug_extension_mv3/web/settings.dart
+++ dwds/debug_extension_mv3/web/settings.dart
@@ -33,6 +33,7 @@ void _updateSettingsFromStorage(Event _) async {
 
 void _saveSettingsToStorage(Event event) async {
   event.preventDefault();
+  _maybeHideSavedMsg();
   final form = document.querySelector("form") as FormElement;
   final data = FormData(form);
   final devToolsOpenerValue = data.get('devToolsOpener') as String;
@@ -44,14 +45,18 @@ void _saveSettingsToStorage(Event event) async {
 }
 
 void _showSavedMsg() {
-  final msgContainer = document.getElementById('savedMsgEmpty');
-  if (msgContainer == null) return;
-  msgContainer.id = 'savedMsg';
-  msgContainer.innerHtml = 'Saved!';
+  final snackbar = document.getElementById('savedSnackbar');
+  if (snackbar == null) return;
+  snackbar.classes.add('show');
   Timer(Duration(seconds: 3), () {
-    msgContainer.id = 'savedMsgEmpty';
-    msgContainer.innerHtml = '';
+    _maybeHideSavedMsg();
   });
+}
+
+void _maybeHideSavedMsg() {
+  final snackbar = document.getElementById('savedSnackbar');
+  if (snackbar == null) return;
+  snackbar.classes.remove('show');
 }
 
 RadioButtonInputElement _getRadioButton(String id) {
