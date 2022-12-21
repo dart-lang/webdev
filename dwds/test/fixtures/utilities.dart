8285538c206fbe735ac6c7a51ca70deb9a81e7b7
Elliott Brooks <21270878+elliette@users.noreply.github.com>
2022-12-21 09:42:48 -0800

    Pull 'webdev' into variable

--- dwds/test/fixtures/utilities.dart
+++ dwds/test/fixtures/utilities.dart
@@ -9,14 +9,20 @@ import 'package:build_daemon/constants.dart';
 import 'package:build_daemon/data/server_log.dart';
 import 'package:path/path.dart' as p;
 
+const webdevDirName = 'webdev';
+const dwdsDirName = 'dwds';
+
 /// The path to the DWDS directory in the local machine, e.g.
 /// "/workstation/webdev/dwds".
 String get dwdsPath {
   final pathParts = p.split(p.current);
   // We expect all tests to be run from the webdev mono-repo:
-  assert(pathParts.contains('webdev'));
+  assert(pathParts.contains(webdevDirName));
   return p.joinAll(
-    [...pathParts.sublist(0, pathParts.lastIndexOf('webdev') + 1), 'dwds'],
+    [
+      ...pathParts.sublist(0, pathParts.lastIndexOf(webdevDirName) + 1),
+      dwdsDirName,
+    ],
   );
 }
 
