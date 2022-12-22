8795ca5b89df0f912e31b0f5fa372b90f2a0bf66
Elliott Brooks (she/her) <21270878+elliette@users.noreply.github.com>
2022-12-22 13:11:44 -0800

    Tests should serve`main.dart` and `index.html` from the same directory (#1856)

--- dwds/test/variable_scope_test.dart
+++ dwds/test/variable_scope_test.dart
@@ -7,13 +7,24 @@
 import 'package:dwds/src/connections/debug_connection.dart';
 import 'package:dwds/src/debugging/dart_scope.dart';
 import 'package:dwds/src/services/chrome_proxy_service.dart';
+import 'package:path/path.dart' as p;
 import 'package:test/test.dart';
 import 'package:vm_service/vm_service.dart';
 import 'package:webkit_inspection_protocol/webkit_inspection_protocol.dart';
 
 import 'fixtures/context.dart';
 
-final context = TestContext(path: 'scopes/scopes.html');
+final context = TestContext(
+    path: 'scopes/scopes.html',
+    entry: p.join(
+      '..',
+      'fixtures',
+      '_testSound',
+      'example',
+      'scopes',
+      'scopes_main.dart',
+    ));
+
 ChromeProxyService get service =>
     fetchChromeProxyService(context.debugConnection);
 WipConnection get tabConnection => context.tabConnection;
