5dcff91d27bf1e9975c047a63508aff60492e746
Elliott Brooks <21270878+elliette@users.noreply.github.com>
2022-12-22 11:47:46 -0800

    index.html and main.dart file should be in same directory

--- dwds/test/instance_test.dart
+++ dwds/test/instance_test.dart
@@ -8,13 +8,23 @@ import 'package:dwds/src/connections/debug_connection.dart';
 import 'package:dwds/src/debugging/debugger.dart';
 import 'package:dwds/src/debugging/inspector.dart';
 import 'package:dwds/src/loaders/strategy.dart';
+import 'package:path/path.dart' as p;
 import 'package:test/test.dart';
 import 'package:vm_service/vm_service.dart';
 import 'package:webkit_inspection_protocol/webkit_inspection_protocol.dart';
 
 import 'fixtures/context.dart';
 
-final context = TestContext(path: 'scopes/scopes.html');
+final context = TestContext(
+    path: 'scopes/scopes.html',
+    entry: p.join(
+      '..',
+      'fixtures',
+      '_testSound',
+      'example',
+      'scopes',
+      'scopes_main.dart',
+    ));
 
 WipConnection get tabConnection => context.tabConnection;
 
