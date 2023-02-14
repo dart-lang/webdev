86c5602d5433d6790cbcbd30abf1e9becc4cc83b
Elliott Brooks <21270878+elliette@users.noreply.github.com>
2023-02-14 09:49:54 -0800

    Respond to PR comments

--- dwds/debug_extension_mv3/tool/build_extension.dart
+++ dwds/debug_extension_mv3/tool/build_extension.dart
@@ -18,22 +18,22 @@ import 'dart:io';
 import 'package:args/args.dart';
 import 'package:path/path.dart' as p;
 
-const prodFlag = 'prod';
+const _prodFlag = 'prod';
 
 void main(List<String> arguments) async {
   final parser = ArgParser()
-    ..addFlag(prodFlag, negatable: true, defaultsTo: false);
+    ..addFlag(_prodFlag, negatable: true, defaultsTo: false);
   final argResults = parser.parse(arguments);
 
-  exitCode = await run(isProd: argResults[prodFlag] as bool);
+  exitCode = await run(isProd: argResults[_prodFlag] as bool);
   if (exitCode != 0) {
-    logWarning('Run terminated unexpectedly with exit code: $exitCode');
+    _logWarning('Run terminated unexpectedly with exit code: $exitCode');
   }
 }
 
 Future<int> run({required bool isProd}) async {
-  logStep('Building extension for ${isProd ? 'prod' : 'dev'}');
-  logStep('Compiling extension with dart2js to /compiled directory');
+  _logInfo('Building extension for ${isProd ? 'prod' : 'dev'}');
+  _logInfo('Compiling extension with dart2js to /compiled directory');
   final compileStep = await Process.start(
     'dart',
     ['run', 'build_runner', 'build', 'web', '--output', 'build', '--release'],
@@ -43,50 +43,49 @@ Future<int> run({required bool isProd}) async {
   if (compileExitCode != 0) {
     return compileExitCode;
   }
-  logStep('Updating manifest.json in /compiled directory.');
+  _logInfo('Updating manifest.json in /compiled directory.');
   final updateStep = await Process.start(
     'dart',
     [p.join('tool', 'update_dev_files.dart')],
   );
   final updateExitCode = await _handleProcess(updateStep);
-  // Terminate early if updating dev files failed:
-  if (updateExitCode != 0) {
-    return updateExitCode;
-  }
-  // Return 0 to indicate success:
-  return 0;
+  // Return exit code (0 indicates success):
+  return updateExitCode;
 }
 
 Future<int> _handleProcess(Process process) async {
-  _handleOutput(process.stdout);
-  _handleOutput(process.stderr);
+  _handleOutput(process.stdout, isStdout: true);
+  _handleOutput(process.stderr, isStdout: false);
   return process.exitCode;
 }
 
-void _handleOutput(Stream<List<int>> output) {
+void _handleOutput(Stream<List<int>> output, {bool isStdout = true}) {
   output
       .transform(utf8.decoder)
       .transform(const LineSplitter())
-      .listen(_handleOutputLine);
+      .listen((line) => _handleOutputLine(line, isStdout: isStdout));
 }
 
 void _handleOutputLine(String line, {bool isStdout = true}) {
+  // Skip empty lines:
+  if (line.isEmpty) return;
+  // Log any unexpected errors and throw:
   final outputName = isStdout ? 'stdout' : 'stderr';
   if (line.toUpperCase().contains('SEVERE') ||
       line.toUpperCase().contains('ERROR')) {
     final error = 'Unexpected error in $outputName: $line';
-    logWarning(error);
+    _logWarning(error);
     throw Exception(error);
   }
-  if (line.isNotEmpty) {
-    print('$outputName: $line');
-  }
+  // Log message to the terminal:
+  final message = '$outputName: $line';
+  isStdout ? _logInfo(message) : _logWarning(message);
 }
 
-void logStep(String message) {
-  print('[BUILD STEP] $message');
+void _logInfo(String message) {
+  stdout.writeln(message);
 }
 
-void logWarning(String message) {
-  print('[WARNING] $message');
+void _logWarning(String warning) {
+  stderr.writeln(warning);
 }
