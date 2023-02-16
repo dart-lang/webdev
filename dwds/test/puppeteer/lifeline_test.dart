6ade4e612967acd871387a54d423932ae9514de3
Elliott Brooks <21270878+elliette@users.noreply.github.com>
2023-02-15 16:21:15 -0800

    Fix analyze error

--- dwds/test/puppeteer/lifeline_test.dart
+++ dwds/test/puppeteer/lifeline_test.dart
@@ -52,7 +52,7 @@ void main() async {
         }
       });
       // Click on the Dart Debug Extension icon to intiate a debug session:
-      await clickOnExtensionIcon(worker);
+      await clickOnExtensionIcon(worker: worker, backgroundPage: null);
       final connectedToPort = await portConnectionFuture;
       // Verify that we have connected to the port:
       expect(connectedToPort, isTrue);
