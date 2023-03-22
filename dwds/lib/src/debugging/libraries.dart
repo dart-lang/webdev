26edfb84af72806f44b8b741dc702ccc2c04f27f
Elliott Brooks <21270878+elliette@users.noreply.github.com>
2023-03-22 14:41:01 -0700

    Open correct entrypoint file

--- dwds/lib/src/debugging/libraries.dart
+++ dwds/lib/src/debugging/libraries.dart
@@ -38,12 +38,13 @@ class LibraryHelper extends Domain {
     if (_loadStrategy.appEntrypoint != null) {
       _rootLib = libraries.firstWhereOrNull(
           (lib) => Uri.parse(lib.uri ?? '') == _loadStrategy.appEntrypoint);
+    } else {
+      _rootLib = libraries
+          .firstWhereOrNull((lib) => lib.name?.contains('org-dartlang') ?? false);
+      _rootLib = _rootLib ??
+          libraries
+              .firstWhereOrNull((lib) => lib.name?.contains('main') ?? false);
     }
-    _rootLib = libraries
-        .firstWhereOrNull((lib) => lib.name?.contains('org-dartlang') ?? false);
-    _rootLib = _rootLib ??
-        libraries
-            .firstWhereOrNull((lib) => lib.name?.contains('main') ?? false);
     _rootLib = _rootLib ?? (libraries.isNotEmpty ? libraries.last : null);
     return _rootLib!;
   }
