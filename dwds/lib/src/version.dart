04463228109627a41b9fc0475c0cad71b253a0e3
Anna Gringauze <annagrin@google.com>
2023-07-05 14:17:10 -0700

    Merge with master

--- dwds/lib/src/version.dart
+++ dwds/lib/src/version.dart
@@ -1,6 +1,2 @@
 // Generated code. Do not modify.
-<<<<<<< HEAD
 const packageVersion = '20.0.0-wip';
-=======
-const packageVersion = '19.0.3-wip';
->>>>>>> b672e98a4f1071bace009da84b6544151977a2b6
