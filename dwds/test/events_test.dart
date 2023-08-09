ecdb52030971bf5b18e83662bc343946a029834a
Elliott Brooks <21270878+elliette@users.noreply.github.com>
2023-08-09 16:07:10 -0700

    Re-enable events_test with fix

--- dwds/test/events_test.dart
+++ dwds/test/events_test.dart
@@ -160,7 +160,6 @@ void main() {
             () => keyboard.sendChord([Keyboard.alt, 'd']),
           );
         },
-        skip: 'https://github.com/dart-lang/webdev/issues/2181',
       );
 
       test('emits DEVTOOLS_LAUNCH event', () async {
