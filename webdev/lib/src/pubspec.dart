267aeb69af495f62087fd253aa0849f8e00d548e
Parker Lougheed <parlough@gmail.com>
2023-09-06 23:02:09 -0500

    Run formatter

--- webdev/lib/src/pubspec.dart
+++ webdev/lib/src/pubspec.dart
@@ -198,8 +198,7 @@ class _PackageInfo {
 
 /// Returns the package info for the latest webdev release.
 Future<_PackageInfo> _latestPackageInfo() async {
-  var response = await get(
-      Uri.parse('https://pub.dev/api/packages/webdev'),
+  var response = await get(Uri.parse('https://pub.dev/api/packages/webdev'),
       headers: {HttpHeaders.userAgentHeader: 'webdev $packageVersion'});
   var responseObj = json.decode(response.body);
   var pubspec = Pubspec.fromJson(
