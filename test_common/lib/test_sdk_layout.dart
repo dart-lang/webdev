cc66e55917a8ab722f34d6daa3873dd233f03ad5
MarkZ <markzipan@google.com>
2023-12-04 16:08:59 -0800

    Updating test sdk paths

--- test_common/lib/test_sdk_layout.dart
+++ test_common/lib/test_sdk_layout.dart
@@ -32,140 +32,185 @@ class TestSdkLayout {
       TestSdkLayout.createDdcDefaultFromSdkLayout(
           SdkLayout.createDefault(sdkDirectory));
 
-  factory TestSdkLayout.createDefaultFromSdkLayout(SdkLayout sdkLayout) {
-    return TestSdkLayout(
-      sdkDirectory: sdkLayout.sdkDirectory,
-      soundSummaryPath: sdkLayout.soundSummaryPath,
-      soundFullDillPath: p.join(
-        sdkLayout.sdkDirectory,
-        'lib',
-        '_internal',
-        'ddc_platform.dill',
-      ),
-      soundJsPath: p.join(sdkLayout.sdkDirectory,
-          '../gen/utils/ddc/stable/sdk/amd/dart_sdk.js'),
-      soundJsMapPath: p.join(sdkLayout.sdkDirectory,
-          '../gen/utils/ddc/stable/sdk/amd/dart_sdk.js.map'),
-      weakSummaryPath: sdkLayout.weakSummaryPath,
-      weakFullDillPath: p.join(
-        sdkLayout.sdkDirectory,
-        'lib',
-        '_internal',
-        'ddc_platform_unsound.dill',
-      ),
-      weakJsPath: p.join(sdkLayout.sdkDirectory,
-          '../gen/utils/ddc/stable_unsound/sdk/amd/dart_sdk.js'),
-      weakJsMapPath: p.join(sdkLayout.sdkDirectory,
-          '../gen/utils/ddc/stable_unsound/sdk/amd/dart_sdk.js.map'),
-      ddcModuleLoaderJsPath: '',
-      requireJsPath: p.join(
-        sdkLayout.sdkDirectory,
-        'lib',
-        'dev_compiler',
-        'amd',
-        'require.js',
-      ),
-      stackTraceMapperPath: p.join(
-        sdkLayout.sdkDirectory,
-        'lib',
-        'dev_compiler',
-        'web',
-        'dart_stack_trace_mapper.js',
-      ),
-      dartPath: p.join(
-        sdkLayout.sdkDirectory,
-        'bin',
-        Platform.isWindows ? 'dart.exe' : 'dart',
-      ),
-      frontendServerSnapshotPath: p.join(
-        sdkLayout.sdkDirectory,
-        'bin',
-        'snapshots',
-        'frontend_server.dart.snapshot',
-      ),
-      dartdevcSnapshotPath: sdkLayout.dartdevcSnapshotPath,
-      kernelWorkerSnapshotPath: p.join(
-        sdkLayout.sdkDirectory,
-        'bin',
-        'snapshots',
-        'kernel_worker.dart.snapshot',
-      ),
-      devToolsDirectory: p.join(
-        sdkLayout.sdkDirectory,
-        'bin',
-        'resources',
-        'devtools',
-      ),
-    );
-  }
+  factory TestSdkLayout.createDefaultFromSdkLayout(SdkLayout sdkLayout) =>
+      TestSdkLayout(
+        sdkDirectory: sdkLayout.sdkDirectory,
+        soundSummaryPath: sdkLayout.soundSummaryPath,
+        soundFullDillPath: p.join(
+          sdkLayout.sdkDirectory,
+          'lib',
+          '_internal',
+          'ddc_platform.dill',
+        ),
+        soundJsPath: p.join(
+          sdkLayout.sdkDirectory,
+          'lib',
+          'dev_compiler',
+          'kernel',
+          'amd',
+          'dart_sdk.js',
+        ),
+        soundJsMapPath: p.join(
+          sdkLayout.sdkDirectory,
+          'lib',
+          'dev_compiler',
+          'kernel',
+          'amd',
+          'dart_sdk.js.map',
+        ),
+        weakSummaryPath: sdkLayout.weakSummaryPath,
+        weakFullDillPath: p.join(
+          sdkLayout.sdkDirectory,
+          'lib',
+          '_internal',
+          'ddc_platform_unsound.dill',
+        ),
+        weakJsPath: p.join(
+          sdkLayout.sdkDirectory,
+          'lib',
+          'dev_compiler',
+          'kernel',
+          'amd',
+          'dart_sdk_unsound.js',
+        ),
+        weakJsMapPath: p.join(
+          sdkLayout.sdkDirectory,
+          'lib',
+          'dev_compiler',
+          'kernel',
+          'amd',
+          'dart_sdk_unsound.js.map',
+        ),
+        ddcModuleLoaderJsPath: '',
+        requireJsPath: p.join(
+          sdkLayout.sdkDirectory,
+          'lib',
+          'dev_compiler',
+          'amd',
+          'require.js',
+        ),
+        stackTraceMapperPath: p.join(
+          sdkLayout.sdkDirectory,
+          'lib',
+          'dev_compiler',
+          'web',
+          'dart_stack_trace_mapper.js',
+        ),
+        dartPath: p.join(
+          sdkLayout.sdkDirectory,
+          'bin',
+          Platform.isWindows ? 'dart.exe' : 'dart',
+        ),
+        frontendServerSnapshotPath: p.join(
+          sdkLayout.sdkDirectory,
+          'bin',
+          'snapshots',
+          'frontend_server.dart.snapshot',
+        ),
+        dartdevcSnapshotPath: sdkLayout.dartdevcSnapshotPath,
+        kernelWorkerSnapshotPath: p.join(
+          sdkLayout.sdkDirectory,
+          'bin',
+          'snapshots',
+          'kernel_worker.dart.snapshot',
+        ),
+        devToolsDirectory: p.join(
+          sdkLayout.sdkDirectory,
+          'bin',
+          'resources',
+          'devtools',
+        ),
+      );
 
-  factory TestSdkLayout.createDdcDefaultFromSdkLayout(SdkLayout sdkLayout) {
-    final sdkPathSegment = 'legacy';
-    return TestSdkLayout(
-      sdkDirectory: sdkLayout.sdkDirectory,
-      soundSummaryPath: sdkLayout.soundSummaryPath,
-      soundFullDillPath: p.join(
-        sdkLayout.sdkDirectory,
-        'lib',
-        '_internal',
-        'ddc_platform.dill',
-      ),
-      soundJsPath: p.join(sdkLayout.sdkDirectory,
-          '../gen/utils/ddc/stable/sdk/$sdkPathSegment/dart_sdk.js'),
-      soundJsMapPath: p.join(sdkLayout.sdkDirectory,
-          '../gen/utils/ddc/stable/sdk/$sdkPathSegment/dart_sdk.js.map'),
-      weakSummaryPath: sdkLayout.weakSummaryPath,
-      weakFullDillPath: p.join(
-        sdkLayout.sdkDirectory,
-        'lib',
-        '_internal',
-        'ddc_platform_unsound.dill',
-      ),
-      weakJsPath: p.join(sdkLayout.sdkDirectory,
-          '../gen/utils/ddc/stable_unsound/sdk/$sdkPathSegment/dart_sdk.js'),
-      weakJsMapPath: p.join(sdkLayout.sdkDirectory,
-          '../gen/utils/ddc/stable_unsound/sdk/$sdkPathSegment/dart_sdk.js.map'),
-      ddcModuleLoaderJsPath: p.join(
-        sdkLayout.sdkDirectory,
-        'lib',
-        'dev_compiler',
-        'ddc',
-        'ddc_module_loader.js',
-      ),
-      requireJsPath: '',
-      stackTraceMapperPath: p.join(
-        sdkLayout.sdkDirectory,
-        'lib',
-        'dev_compiler',
-        'web',
-        'dart_stack_trace_mapper.js',
-      ),
-      dartPath: p.join(
-        sdkLayout.sdkDirectory,
-        'bin',
-        Platform.isWindows ? 'dart.exe' : 'dart',
-      ),
-      frontendServerSnapshotPath: p.join(
-        sdkLayout.sdkDirectory,
-        'bin',
-        'snapshots',
-        'frontend_server.dart.snapshot',
-      ),
-      dartdevcSnapshotPath: sdkLayout.dartdevcSnapshotPath,
-      kernelWorkerSnapshotPath: p.join(
-        sdkLayout.sdkDirectory,
-        'bin',
-        'snapshots',
-        'kernel_worker.dart.snapshot',
-      ),
-      devToolsDirectory: p.join(
-        sdkLayout.sdkDirectory,
-        'bin',
-        'resources',
-        'devtools',
-      ),
-    );
-  }
+  factory TestSdkLayout.createDdcDefaultFromSdkLayout(SdkLayout sdkLayout) =>
+      TestSdkLayout(
+        sdkDirectory: sdkLayout.sdkDirectory,
+        soundSummaryPath: sdkLayout.soundSummaryPath,
+        soundFullDillPath: p.join(
+          sdkLayout.sdkDirectory,
+          'lib',
+          '_internal',
+          'ddc_platform.dill',
+        ),
+        soundJsPath: p.join(
+          sdkLayout.sdkDirectory,
+          'lib',
+          'dev_compiler',
+          'kernel',
+          'ddc',
+          'dart_sdk.js',
+        ),
+        soundJsMapPath: p.join(
+          sdkLayout.sdkDirectory,
+          'lib',
+          'dev_compiler',
+          'kernel',
+          'ddc',
+          'dart_sdk.js.map',
+        ),
+        weakSummaryPath: sdkLayout.weakSummaryPath,
+        weakFullDillPath: p.join(
+          sdkLayout.sdkDirectory,
+          'lib',
+          '_internal',
+          'ddc_platform_unsound.dill',
+        ),
+        weakJsPath: p.join(
+          sdkLayout.sdkDirectory,
+          'lib',
+          'dev_compiler',
+          'kernel',
+          'ddc',
+          'dart_sdk_unsound.js',
+        ),
+        weakJsMapPath: p.join(
+          sdkLayout.sdkDirectory,
+          'lib',
+          'dev_compiler',
+          'kernel',
+          'ddc',
+          'dart_sdk_unsound.js.map',
+        ),
+        ddcModuleLoaderJsPath: p.join(
+          sdkLayout.sdkDirectory,
+          'lib',
+          'dev_compiler',
+          'ddc',
+          'ddc_module_loader.js',
+        ),
+        requireJsPath: '',
+        stackTraceMapperPath: p.join(
+          sdkLayout.sdkDirectory,
+          'lib',
+          'dev_compiler',
+          'web',
+          'dart_stack_trace_mapper.js',
+        ),
+        dartPath: p.join(
+          sdkLayout.sdkDirectory,
+          'bin',
+          Platform.isWindows ? 'dart.exe' : 'dart',
+        ),
+        frontendServerSnapshotPath: p.join(
+          sdkLayout.sdkDirectory,
+          'bin',
+          'snapshots',
+          'frontend_server.dart.snapshot',
+        ),
+        dartdevcSnapshotPath: sdkLayout.dartdevcSnapshotPath,
+        kernelWorkerSnapshotPath: p.join(
+          sdkLayout.sdkDirectory,
+          'bin',
+          'snapshots',
+          'kernel_worker.dart.snapshot',
+        ),
+        devToolsDirectory: p.join(
+          sdkLayout.sdkDirectory,
+          'bin',
+          'resources',
+          'devtools',
+        ),
+      );
 
   final String sdkDirectory;
 
