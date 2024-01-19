109ea6c015594ecab95e504f00e384d5773b4959
MarkZ <markzipan@google.com>
2024-01-19 10:50:43 -0800

    Removing library statements

--- fixtures/_testHotRestart1Sound/lib/library1.dart
+++ fixtures/_testHotRestart1Sound/lib/library1.dart
@@ -2,8 +2,6 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-library _test_hot_restart1;
-
 class B {
   final int x;
   const B(this.x);
