b8d31a88d58ba8b10bf7eef2efaa7700761b5cf1
Derek Xu <derekx@google.com>
2024-09-09 11:33:51 -0400

    Migrate injected client to package:web

--- dwds/web/run_main.dart
+++ dwds/web/run_main.dart
@@ -2,17 +2,8 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-import 'dart:html';
-
-/// Creates a script that will run properly when strict CSP is enforced.
-///
-/// More specifically, the script has the correct `nonce` value set.
-final ScriptElement Function() _createScript = (() {
-  final nonce = _findNonce();
-  if (nonce == null) return ScriptElement.new;
-
-  return () => ScriptElement()..setAttribute('nonce', nonce);
-})();
+import 'dart:js_interop';
+import 'package:web/helpers.dart';
 
 // According to the CSP3 spec a nonce must be a valid base64 string.
 final _noncePattern = RegExp('^[\\w+/_-]+[=]{0,2}\$');
@@ -20,22 +11,40 @@ final _noncePattern = RegExp('^[\\w+/_-]+[=]{0,2}\$');
 /// Returns CSP nonce, if set for any script tag.
 String? _findNonce() {
   final elements = window.document.querySelectorAll('script');
-  for (final element in elements) {
-    final nonceValue =
-        (element as HtmlElement).nonce ?? element.attributes['nonce'];
-    if (nonceValue != null && _noncePattern.hasMatch(nonceValue)) {
+
+  for (var i = 0; i < elements.length; i++) {
+    final element = elements.item(i);
+    final nonceValue = (element as HtmlElement).nonce;
+    if (_noncePattern.hasMatch(nonceValue)) {
       return nonceValue;
     }
   }
   return null;
 }
 
+/// Creates a script that will run properly when strict CSP is enforced.
+///
+/// More specifically, the script has the correct `nonce` value set.
+final HTMLElement Function() _createScript = (() {
+  final nonce = _findNonce();
+
+  if (nonce == null) {
+    return () => document.createElement('script') as HTMLElement;
+  }
+  return () {
+    final scriptElement = document.createElement('script') as HTMLElement;
+    return scriptElement..setAttribute('nonce', nonce);
+  };
+})();
+
 /// Runs `window.$dartRunMain()` by injecting a script tag.
 ///
 /// We do this so that we don't see user exceptions bubble up in our own error
 /// handling zone.
 void runMain() {
-  final scriptElement = _createScript()..innerHtml = r'window.$dartRunMain();';
-  document.body!.append(scriptElement);
-  Future.microtask(scriptElement.remove);
+  final scriptElement = _createScript()..innerHTML = r'window.$dartRunMain();';
+  document.body!.append(scriptElement.jsify()!);
+  // External tear-offs are not allowed.
+  // ignore: unnecessary_lambdas
+  Future.microtask(() => scriptElement.remove());
 }
