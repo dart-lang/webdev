4008bce4c22745195638e5d25c24ea030b905531
Srujan Gaddam <srujzs@google.com>
2025-01-20 10:44:45 -0800

    Implement hot restart for DDC library bundle format
    - Optionally invokes ext.flutter.disassemble to restart engine
    - Calls dartDevEmbedder.hotRestart to trigger a hot restart
    
    The rest of the implementation will and needs to be in the Flutter
    tools repo.

--- dwds/web/reloader/ddc_library_bundle_restarter.dart
+++ dwds/web/reloader/ddc_library_bundle_restarter.dart
@@ -3,14 +3,35 @@
 // BSD-style license that can be found in the LICENSE file.
 
 import 'dart:async';
+import 'dart:js_interop';
 
 import 'restarter.dart';
+
+@JS('dartDevEmbedder')
+external _DartDevEmbedder get _dartDevEmbedder;
+
+extension type _DartDevEmbedder._(JSObject _) implements JSObject {
+  external _Debugger get debugger;
+  external JSPromise<JSAny?> hotRestart();
+}
+
+extension type _Debugger._(JSObject _) implements JSObject {
+  external JSPromise<JSString> invokeExtension(String key, String params);
+  external JSArray<JSString> get extensionNames;
+
+  Future<void> maybeInvokeFlutterDisassemble() async {
+    final method = 'ext.flutter.disassemble';
+    if (extensionNames.toDart.contains(method.toJS)) {
+      await invokeExtension(method, '{}').toDart;
+    }
+  }
+}
 
 class DdcLibraryBundleRestarter implements Restarter {
   @override
   Future<bool> restart({String? runId, Future? readyToRunMain}) async {
-    throw UnimplementedError(
-      "Hot restart isn't supported for the DDC library bundle format yet.",
-    );
+    await _dartDevEmbedder.debugger.maybeInvokeFlutterDisassemble();
+    await _dartDevEmbedder.hotRestart().toDart;
+    return true;
   }
 }
