0c8a17b4b1cb3d128bd982a2b58b79ca1a6450ec
Morgan :) <davidmorgan@google.com>
2025-06-20 08:53:33 +0200

    Remove dependency overrides. (#2634)
    * Remove dependency overrides.
    
    * Address review comments.
    
    * Address review comments.

--- webdev/test/e2e_test.dart
+++ webdev/test/e2e_test.dart
@@ -10,6 +10,7 @@ import 'dart:io';
 import 'package:io/io.dart';
 import 'package:logging/logging.dart';
 import 'package:path/path.dart' as p;
+import 'package:pub_semver/pub_semver.dart';
 import 'package:test/test.dart';
 import 'package:test_common/utilities.dart';
 import 'package:test_descriptor/test_descriptor.dart' as d;
@@ -69,8 +70,10 @@ void main() {
         loadYaml(await File('pubspec.yaml').readAsString()) as YamlMap;
     expect(smokeYaml['environment']['sdk'],
         equals(webdevYaml['environment']['sdk']));
-    expect(smokeYaml['dev_dependencies']['build_runner'],
-        equals(buildRunnerConstraint.toString()));
+    expect(
+        buildRunnerConstraint.allowsAny(VersionConstraint.parse(
+            smokeYaml['dev_dependencies']['build_runner'])),
+        true);
     expect(smokeYaml['dev_dependencies']['build_web_compilers'],
         equals(buildWebCompilersConstraint.toString()));
   });
