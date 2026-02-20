021c78fad2299edf99409d80d5f7bb8f8a5f8bc8
Jessy Yameogo <yjessy@google.com>
2026-02-20 16:40:31 -0500

    applied dart format

--- dwds/test/utilities/uuid_test.dart
+++ dwds/test/utilities/uuid_test.dart
@@ -12,9 +12,13 @@ void main() {
       final id = uuid.v4();
       expect(id, hasLength(36));
       expect(
-          id,
-          matches(
-              RegExp(r'^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$')));
+        id,
+        matches(
+          RegExp(
+            r'^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$',
+          ),
+        ),
+      );
     });
 
     test('v4 generates unique UUIDs', () {
