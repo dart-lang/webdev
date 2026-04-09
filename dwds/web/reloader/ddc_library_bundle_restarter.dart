b65fcfdbe93ff82ad7167a190ef53a4708863f2f
MarkZ <markzipan@google.com>
2026-04-08 19:16:47 -0700

    updating return values

--- dwds/web/reloader/ddc_library_bundle_restarter.dart
+++ dwds/web/reloader/ddc_library_bundle_restarter.dart
@@ -103,7 +103,7 @@ class DdcLibraryBundleRestarter implements Restarter {
   }
 
   @override
-  Future<(bool, JSArray<JSObject>?)> restart({
+  Future<(bool, JSArray<JSObject>)> restart({
     String? runId,
     Future? readyToRunMain,
     String? reloadedSourcesPath,
@@ -122,7 +122,7 @@ class DdcLibraryBundleRestarter implements Restarter {
       reloadedSourcesPath!,
     );
     await _dartDevEmbedder.hotRestart().toDart;
-    return (true, srcModuleLibraries.jsify() as JSArray<JSObject>?);
+    return (true, srcModuleLibraries.jsify() as JSArray<JSObject>);
   }
 
   @override
