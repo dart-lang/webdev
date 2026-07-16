858e6f29122980aaae40ecdab7360cd3cf16ea09
MarkZ <markzipan@google.com>
2026-07-16 11:42:28 -0700

    Updating versions

--- webdev/lib/src/pubspec.dart
+++ webdev/lib/src/pubspec.dart
@@ -188,7 +188,7 @@ Future<List<PackageExceptionDetails>> _validateBuildDaemonVersion(
 }
 
 final buildRunnerConstraint = VersionConstraint.parse('^2.4.0');
-final buildWebCompilersConstraint = VersionConstraint.parse('^4.8.7');
+final buildWebCompilersConstraint = VersionConstraint.parse('^4.8.8');
 
 // Note the minimum versions should never be dev versions as users will not
 // get them by default.
