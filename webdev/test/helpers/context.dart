08324db682506316e1d9eea73af5e8a7c737dc5c
MarkZ <markzipan@google.com>
2026-08-15 03:23:27 -0700

    Fix appServerPath in dart_uri_file_uri and reset _expressionCompiler in BuildDaemonTestContext

--- webdev/test/helpers/context.dart
+++ webdev/test/helpers/context.dart
@@ -157,6 +157,8 @@ class BuildDaemonTestContext extends TestContext {
         sdkConfigurationProvider: sdkConfigurationProvider,
       );
       _expressionCompiler = ddcService;
+    } else {
+      _expressionCompiler = null;
     }
 
     _loadStrategy = switch ((
@@ -199,6 +201,8 @@ class BuildDaemonTestContext extends TestContext {
   @override
   Future<void> modeTearDown() async {
     await ddcService?.stop();
+    ddcService = null;
+    _expressionCompiler = null;
     await daemonClient.close();
   }
 }
