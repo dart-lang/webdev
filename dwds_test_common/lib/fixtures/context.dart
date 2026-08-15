d66334403bf88adff2c97593a24f047fdaded3e1
MarkZ <markzipan@google.com>
2026-08-15 01:48:51 -0700

    Pass provider moduleFormat and canaryFeatures to TestSettings in dart_uri_file_uri tests

--- dwds_test_common/lib/fixtures/context.dart
+++ dwds_test_common/lib/fixtures/context.dart
@@ -735,5 +735,12 @@ String _resolveChromeExecutable() {
     }
     return 'chrome.exe';
   }
+  if (Platform.isMacOS) {
+    const defaultMacPath =
+        '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome';
+    if (File(defaultMacPath).existsSync()) {
+      return defaultMacPath;
+    }
+  }
   return _chromeExecutableName;
 }
