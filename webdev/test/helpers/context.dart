d5d4133156d7299ff1301cf15d39adedff2843ef
MarkZ <markzipan@google.com>
2026-08-18 16:22:45 -0700

    Reduce concurrency in asset_handler test to prevent socket exhaustion and add safe daemonClient closing

--- webdev/test/helpers/context.dart
+++ webdev/test/helpers/context.dart
@@ -309,7 +309,9 @@ class BuildDaemonTestContext extends TestContext with BuildDaemonContextMixin {
     await ddcService?.stop();
     ddcService = null;
     _expressionCompiler = null;
-    await daemonClient.close();
+    try {
+      await daemonClient.close();
+    } catch (_) {}
   }
 }
 
@@ -677,7 +679,9 @@ class BuildDaemonAndFrontendServerTestContext extends TestContext
   @override
   Future<void> modeTearDown() async {
     await ddcService?.stop();
-    await daemonClient.close();
+    try {
+      await daemonClient.close();
+    } catch (_) {}
   }
 }
 
